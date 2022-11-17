17083a2eaab44459b2d2d697e862618c73ba30d1
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-18 00:39:00 +0100

    test(backend-api-models): update ResolveApiSchemaHttpReferenceUseCase tests with additional ones

--- packages/backend-api-models/src/jsonSchema/application/useCases/ResolveApiSchemaHttpReferenceUseCase.spec.ts
+++ packages/backend-api-models/src/jsonSchema/application/useCases/ResolveApiSchemaHttpReferenceUseCase.spec.ts
@@ -22,74 +22,222 @@ describe(ResolveApiSchemaHttpReferenceUseCase.name, () => {
   });
 
   describe('.handle', () => {
-    let resolveApiSchemaHttpReferenceQueryFixture: ResolveApiSchemaHttpReferenceQuery;
+    describe('having a ResolveApiSchemaHttpReferenceQuery', () => {
+      let resolveApiSchemaHttpReferenceQueryFixture: ResolveApiSchemaHttpReferenceQuery;
 
-    beforeAll(() => {
-      resolveApiSchemaHttpReferenceQueryFixture = {
-        callback: jest.fn(),
-        schemasRootDirectory: '/root/directory',
-        url: 'https://sample.com/root/directory/schema/path',
-      };
+      beforeAll(() => {
+        resolveApiSchemaHttpReferenceQueryFixture = {
+          schemasRootDirectory: '/root/directory',
+          url: 'https://sample.com/root/directory/schema/path',
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        let fileContentBufferFixture: Buffer;
+        let fileContentFixture: string;
+        let filePathFixture: string;
+
+        beforeAll(async () => {
+          fileContentFixture = 'file content fixture';
+          fileContentBufferFixture = Buffer.from(fileContentFixture);
+          filePathFixture = '/file/path/fixture';
+
+          (
+            path.join as jestMock.Mock<path.PlatformPath['join']>
+          ).mockReturnValueOnce(filePathFixture);
+
+          (
+            fs.readFile as jestMock.Mock<fs.FileHandle['readFile']>
+          ).mockResolvedValueOnce(fileContentBufferFixture);
+
+          result = await resolveApiSchemaHttpReferenceUseCase.handle(
+            resolveApiSchemaHttpReferenceQueryFixture,
+          );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call path.join()', () => {
+          expect(path.join).toHaveBeenCalledTimes(1);
+          expect(path.join).toHaveBeenCalledWith(
+            resolveApiSchemaHttpReferenceQueryFixture.schemasRootDirectory,
+            '',
+            'root',
+            'directory',
+            'schema',
+            'path',
+          );
+        });
+
+        it('should call fs.readFile()', () => {
+          expect(fs.readFile).toHaveBeenCalledTimes(1);
+          expect(fs.readFile).toHaveBeenCalledWith(filePathFixture);
+        });
+
+        it('should return a buffer', () => {
+          expect(result).toBe(fileContentBufferFixture);
+          expect(result).toStrictEqual(Buffer.from(fileContentFixture));
+        });
+      });
+
+      describe('when called, and fs.readFile() throws an Error', () => {
+        let errorFixture: Error;
+
+        let filePathFixture: string;
+
+        let result: unknown;
+
+        beforeAll(async () => {
+          errorFixture = new Error('Error from fs.read()!');
+          filePathFixture = '/file/path/fixture';
+
+          (
+            path.join as jestMock.Mock<path.PlatformPath['join']>
+          ).mockReturnValueOnce(filePathFixture);
+
+          (
+            fs.readFile as jestMock.Mock<fs.FileHandle['readFile']>
+          ).mockRejectedValueOnce(errorFixture);
+
+          try {
+            await resolveApiSchemaHttpReferenceUseCase.handle(
+              resolveApiSchemaHttpReferenceQueryFixture,
+            );
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should throw and error', () => {
+          expect(result).toBe(errorFixture);
+        });
+      });
     });
 
-    describe('when called', () => {
-      let result: unknown;
+    describe('having a ResolveApiSchemaHttpReferenceQuery with a callback', () => {
+      let resolveApiSchemaHttpReferenceQueryFixture: ResolveApiSchemaHttpReferenceQuery;
 
-      let fileContentBufferFixture: Buffer;
-      let fileContentFixture: string;
-      let filePathFixture: string;
-
-      beforeAll(async () => {
-        fileContentFixture = 'file content fixture';
-        fileContentBufferFixture = Buffer.from(fileContentFixture);
-        filePathFixture = '/file/path/fixture';
-
-        (
-          path.join as jestMock.Mock<path.PlatformPath['join']>
-        ).mockReturnValueOnce(filePathFixture);
-
-        (
-          fs.readFile as jestMock.Mock<fs.FileHandle['readFile']>
-        ).mockResolvedValueOnce(fileContentBufferFixture);
-
-        result = await resolveApiSchemaHttpReferenceUseCase.handle(
-          resolveApiSchemaHttpReferenceQueryFixture,
-        );
+      beforeAll(() => {
+        resolveApiSchemaHttpReferenceQueryFixture = {
+          callback: jest.fn(),
+          schemasRootDirectory: '/root/directory',
+          url: 'https://sample.com/root/directory/schema/path',
+        };
       });
 
-      afterAll(() => {
-        jest.clearAllMocks();
+      describe('when called', () => {
+        let result: unknown;
+
+        let fileContentBufferFixture: Buffer;
+        let fileContentFixture: string;
+        let filePathFixture: string;
+
+        beforeAll(async () => {
+          fileContentFixture = 'file content fixture';
+          fileContentBufferFixture = Buffer.from(fileContentFixture);
+          filePathFixture = '/file/path/fixture';
+
+          (
+            path.join as jestMock.Mock<path.PlatformPath['join']>
+          ).mockReturnValueOnce(filePathFixture);
+
+          (
+            fs.readFile as jestMock.Mock<fs.FileHandle['readFile']>
+          ).mockResolvedValueOnce(fileContentBufferFixture);
+
+          result = await resolveApiSchemaHttpReferenceUseCase.handle(
+            resolveApiSchemaHttpReferenceQueryFixture,
+          );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call path.join()', () => {
+          expect(path.join).toHaveBeenCalledTimes(1);
+          expect(path.join).toHaveBeenCalledWith(
+            resolveApiSchemaHttpReferenceQueryFixture.schemasRootDirectory,
+            '',
+            'root',
+            'directory',
+            'schema',
+            'path',
+          );
+        });
+
+        it('should call fs.readFile()', () => {
+          expect(fs.readFile).toHaveBeenCalledTimes(1);
+          expect(fs.readFile).toHaveBeenCalledWith(filePathFixture);
+        });
+
+        it('should call resolveApiSchemaHttpReferenceQuery.callback()', () => {
+          expect(
+            resolveApiSchemaHttpReferenceQueryFixture.callback,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            resolveApiSchemaHttpReferenceQueryFixture.callback,
+          ).toHaveBeenCalledWith(null, fileContentFixture);
+        });
+
+        it('should return a buffer', () => {
+          expect(result).toBe(fileContentBufferFixture);
+          expect(result).toStrictEqual(Buffer.from(fileContentFixture));
+        });
       });
 
-      it('should call path.join()', () => {
-        expect(path.join).toHaveBeenCalledTimes(1);
-        expect(path.join).toHaveBeenCalledWith(
-          resolveApiSchemaHttpReferenceQueryFixture.schemasRootDirectory,
-          '',
-          'root',
-          'directory',
-          'schema',
-          'path',
-        );
-      });
+      describe('when called, and fs.readFile() throws an Error', () => {
+        let errorFixture: Error;
 
-      it('should call fs.readFile()', () => {
-        expect(fs.readFile).toHaveBeenCalledTimes(1);
-        expect(fs.readFile).toHaveBeenCalledWith(filePathFixture);
-      });
+        let filePathFixture: string;
 
-      it('should call resolveApiSchemaHttpReferenceQuery.callback()', () => {
-        expect(
-          resolveApiSchemaHttpReferenceQueryFixture.callback,
-        ).toHaveBeenCalledTimes(1);
-        expect(
-          resolveApiSchemaHttpReferenceQueryFixture.callback,
-        ).toHaveBeenCalledWith(null, fileContentFixture);
-      });
+        let result: unknown;
 
-      it('should return a buffer', () => {
-        expect(result).toBe(fileContentBufferFixture);
-        expect(result).toStrictEqual(Buffer.from(fileContentFixture));
+        beforeAll(async () => {
+          errorFixture = new Error('Error from fs.read()!');
+          filePathFixture = '/file/path/fixture';
+
+          (
+            path.join as jestMock.Mock<path.PlatformPath['join']>
+          ).mockReturnValueOnce(filePathFixture);
+
+          (
+            fs.readFile as jestMock.Mock<fs.FileHandle['readFile']>
+          ).mockRejectedValueOnce(errorFixture);
+
+          try {
+            await resolveApiSchemaHttpReferenceUseCase.handle(
+              resolveApiSchemaHttpReferenceQueryFixture,
+            );
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call callback()', () => {
+          expect(
+            resolveApiSchemaHttpReferenceQueryFixture.callback,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            resolveApiSchemaHttpReferenceQueryFixture.callback,
+          ).toHaveBeenCalledWith(errorFixture, null);
+        });
+
+        it('should throw and error', () => {
+          expect(result).toBe(errorFixture);
+        });
       });
     });
   });
