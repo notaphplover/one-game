3c3ab4d7e4e19492582feaa51aa6688151d6690b
notaphplover <roberto.pintos.lopez@gmail.com>
2022-11-17 14:13:27 +0100

    feat(backend-common): add UseCase

--- packages/backend-common/src/index.ts
+++ packages/backend-common/src/index.ts
@@ -1,7 +1,8 @@
-import { Controller } from "./common/application/modules/Controller";
-import { Port } from "./common/application/modules/Port";
-import { Entity } from "./common/domain/models/Entity";
-import { Builder } from "./common/domain/modules/Builder";
-import { BuilderAsync } from "./common/domain/modules/BuilderAsync";
+import { Controller } from './common/application/modules/Controller';
+import { Port } from './common/application/modules/Port';
+import { UseCase } from './common/application/modules/UseCase';
+import { Entity } from './common/domain/models/Entity';
+import { Builder } from './common/domain/modules/Builder';
+import { BuilderAsync } from './common/domain/modules/BuilderAsync';
 
-export type { Controller, Builder, BuilderAsync, Entity, Port };
+export type { Controller, Builder, BuilderAsync, Entity, Port, UseCase };
