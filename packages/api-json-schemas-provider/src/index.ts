e9c6ee162f36f2f8bf909b17db2ae813bd8a5913
notaphplover <roberto.pintos.lopez@gmail.com>
2022-12-05 20:35:30 +0100

    chore(api-json-schemas-provider): Added api-json-schemas-provider

---
+++ packages/api-json-schemas-provider/src/index.ts
@@ -0,0 +1,3 @@
+import { readApiJsonSchemas } from './readApiJsonSchemas';
+
+export { readApiJsonSchemas };
---
+++ packages/api-json-schemas-provider/src/readApiJsonSchemas.ts
@@ -0,0 +1,50 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { promisify } from 'util';
+
+import { JsonRootSchema202012 } from '@one-game-js/json-schema-utils';
+import glob from 'glob';
+
+const globAsPromised: (
+  pattern: string,
+  options?: glob.IOptions | undefined,
+) => Promise<string[]> = promisify(glob);
+
+const JSON_SCHEMAS_BLOB_SUFFIX: string = '**/*.json';
+
+const API_SCHEMAS_PACKAGE: string = '@one-game-js/api-json-schemas';
+const API_SCHEMAS_PACKAGE_SCHEMAS_FOLDER: string = 'schemas';
+
+export async function readApiJsonSchemas(): Promise<JsonRootSchema202012[]> {
+  const schemasPackagePath: string = path.dirname(
+    require.resolve(path.join(API_SCHEMAS_PACKAGE, 'package.json')),
+  );
+
+  const jsonSchemasBlob: string = path.join(
+    schemasPackagePath,
+    API_SCHEMAS_PACKAGE_SCHEMAS_FOLDER,
+    JSON_SCHEMAS_BLOB_SUFFIX,
+  );
+
+  const jsonSchemaPaths: string[] = await globAsPromised(jsonSchemasBlob);
+
+  const jsonSchemas: JsonRootSchema202012[] = await Promise.all(
+    jsonSchemaPaths.map(async (jsonSchemaPath: string) =>
+      parseJsonFile(jsonSchemaPath),
+    ),
+  );
+
+  return jsonSchemas;
+}
+
+async function parseJsonFile(path: string): Promise<JsonRootSchema202012> {
+  const fileContentBuffer: Buffer = await fs.readFile(path);
+
+  const fileStringifiedContent: string = fileContentBuffer.toString();
+
+  const jsonSchema: JsonRootSchema202012 = JSON.parse(
+    fileStringifiedContent,
+  ) as JsonRootSchema202012;
+
+  return jsonSchema;
+}
