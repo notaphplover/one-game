d26a6b81ce065c94d4d6684cf1406f01e7d1d4bd
notaphplover <roberto.pintos.lopez@gmail.com>
2022-12-10 19:55:46 +0100

    feat(backend-api-validators): expose ApiJsonSchemasValidationProvider

--- packages/backend-api-validators/src/index.ts
+++ packages/backend-api-validators/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+import { AjvApiJsonSchemasValidationProvider } from './api/infrastructure/validatorProviders/ajv/AjvApiJsonSchemasValidationProvider';
+import { Validator } from './common/application/modules/Validator';
+
+export { AjvApiJsonSchemasValidationProvider as ApiJsonSchemasValidationProvider };
+
+export type { Validator };
