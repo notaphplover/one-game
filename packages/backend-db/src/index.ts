420cf17b7e41f7476a6ecd1287a8580798bc2ccf
notaphplover <roberto.pintos.lopez@gmail.com>
2023-03-02 17:19:27 +0100

    feat(backend-db): expose TypeOrm services

--- packages/backend-db/src/index.ts
+++ packages/backend-db/src/index.ts
@@ -1,1 +1,14 @@
-export {};
+import { QueryToFindQueryTypeOrmConverter } from './persistence/converters/typeorm/QueryToFindQueryTypeOrmConverter';
+import { DeleteTypeOrmService } from './persistence/services/typeorm/DeleteTypeOrmService';
+import { FindTypeOrmService } from './persistence/services/typeorm/FindTypeOrmService';
+import { InsertTypeOrmService } from './persistence/services/typeorm/InsertTypeOrmService';
+import { UpdateTypeOrmService } from './persistence/services/typeorm/UpdateTypeOrmService';
+
+export type { QueryToFindQueryTypeOrmConverter };
+
+export {
+  DeleteTypeOrmService,
+  FindTypeOrmService,
+  InsertTypeOrmService,
+  UpdateTypeOrmService,
+};
