8de86f15cde8a6f1a603d034818371a09d96cab6
notaphplover <roberto.pintos.lopez@gmail.com>
2023-03-03 19:19:28 +0100

    refactor(backend-http): update SingleEntityGetResponseBuilder to extend ResponseBuilder

--- packages/backend-http/src/http/builders/application/SingleEntityGetResponseBuilder.ts
+++ packages/backend-http/src/http/builders/application/SingleEntityGetResponseBuilder.ts
@@ -1,13 +1,12 @@
-import { Builder } from '@one-game-js/backend-common';
 import httpStatusCodes from 'http-status-codes';
 
 import { Response } from '../../models/application/Response';
 import { ResponseWithBody } from '../../models/application/ResponseWithBody';
+import { ResponseBuilder } from './ResponseBuilder';
 
-export class SingleEntityGetResponseBuilder<TModel>
-  implements
-    Builder<Response | ResponseWithBody<unknown>, [TModel | undefined]>
-{
+export class SingleEntityGetResponseBuilder<TModel> extends ResponseBuilder<
+  [TModel | undefined]
+> {
   public build(
     model: TModel | undefined,
   ): Response | ResponseWithBody<unknown> {
@@ -15,21 +14,17 @@ export class SingleEntityGetResponseBuilder<TModel>
 
     if (model === undefined) {
       httpResponse = {
-        headers: this.#getHttpResponseHeaders(),
+        headers: this._getHttpResponseHeaders(),
         statusCode: httpStatusCodes.NOT_FOUND,
       };
     } else {
       httpResponse = {
         body: model,
-        headers: this.#getHttpResponseHeaders(),
+        headers: this._getHttpResponseHeaders(),
         statusCode: httpStatusCodes.OK,
       };
     }
 
     return httpResponse;
-  }
-
-  #getHttpResponseHeaders(): Record<string, string> {
-    return { 'Content-Type': 'application/json' };
   }
 }
