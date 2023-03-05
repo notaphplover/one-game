6d7dfab6a134a12f588861aa6656919d54dd02e0
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-05 21:48:02 +0100

    refactor(backend-service-user): add EnvironmentLoader

---
+++ packages/backend-service-user/src/foundation/env/services/EnvironmentLoader.ts
@@ -0,0 +1,22 @@
+import { EnvLoader } from '@one-game-js/backend-env';
+import { cleanEnv, json } from 'envalid';
+
+import { Environment } from '../models/application/Environment';
+
+const DOT_ENV_PATH: string = '.env';
+
+export class EnvironmentLoader extends EnvLoader<Environment> {
+  public static build(): EnvironmentLoader {
+    const environmentLoader: EnvironmentLoader = new EnvironmentLoader(
+      DOT_ENV_PATH,
+    );
+
+    return environmentLoader;
+  }
+
+  protected _parseEnv(env: Record<string, string>): Environment {
+    return cleanEnv(env, {
+      typeOrmDatasourceOptions: json(),
+    });
+  }
+}
