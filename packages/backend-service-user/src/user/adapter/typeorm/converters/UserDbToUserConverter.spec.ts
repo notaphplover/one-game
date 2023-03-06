a1d455b71a3174e1f74f0547fa0e440726ac3318
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-06 12:33:11 +0100

    refactor(backend-service-user): add UserDbToUserConverter

---
+++ packages/backend-service-user/src/user/adapter/typeorm/converters/UserDbToUserConverter.spec.ts
@@ -0,0 +1,34 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { User } from '../../../domain/models/User';
+import { UserDbFixtures } from '../fixtures/UserDbFixtures';
+import { UserDb } from '../models/UserDb';
+import { UserDbToUserConverter } from './UserDbToUserConverter';
+
+describe(UserDbToUserConverter.name, () => {
+  let userDbToUserConverter: UserDbToUserConverter;
+
+  beforeAll(() => {
+    userDbToUserConverter = new UserDbToUserConverter();
+  });
+
+  describe('when called', () => {
+    let userDbFixture: UserDb;
+    let result: unknown;
+
+    beforeAll(() => {
+      userDbFixture = UserDbFixtures.any;
+
+      result = userDbToUserConverter.convert(userDbFixture);
+    });
+
+    it('should return a user', () => {
+      const expected: User = {
+        id: userDbFixture.id,
+        name: userDbFixture.name,
+      };
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
---
+++ packages/backend-service-user/src/user/adapter/typeorm/converters/UserDbToUserConverter.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Converter } from '@one-game-js/backend-common';
+
+import { User } from '../../../domain/models/User';
+import { UserDb } from '../models/UserDb';
+
+@Injectable()
+export class UserDbToUserConverter implements Converter<UserDb, User> {
+  public convert(user: UserDb): User {
+    return {
+      id: user.id,
+      name: user.name,
+    };
+  }
+}
