e84f8b7e267519320e206f4f262509b2774ac800
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-07 17:54:12 +0100

    refactor(backend-service-user): add PostUserV1HttpRequestController

--- packages/backend-service-user/src/user/adapter/nest/modules/UserHttpApiModule.ts
+++ packages/backend-service-user/src/user/adapter/nest/modules/UserHttpApiModule.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { HttpModule } from '../../../../foundation/http/adapter/nest/modules/HttpModule';
 import { JsonSchemaModule } from '../../../../foundation/jsonSchema/adapter/nest/modules/JsonSchemaModule';
+import { PostUserV1HttpRequestController } from '../../../application/controllers/PostUserV1HttpRequestController';
 import { PostUserV1RequestParamHandler } from '../../../application/handlers/PostUserV1RequestParamHandler';
+import { UserModule } from './UserModule';
 
 @Module({
-  imports: [JsonSchemaModule],
-  providers: [PostUserV1RequestParamHandler],
+  imports: [JsonSchemaModule, HttpModule, UserModule],
+  providers: [PostUserV1HttpRequestController, PostUserV1RequestParamHandler],
 })
 export class UserHttpApiModule {}
