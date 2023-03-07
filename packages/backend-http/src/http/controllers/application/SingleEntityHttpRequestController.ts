580ac6694f05c4d344bd9fb0be4dedb7eeb576af
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-07 17:50:16 +0100

    refactor(backend-http): update SingleEntityHttpRequestController
    it to no longer has api model
    it relies on an abstract _handleUseCase method

--- packages/backend-http/src/http/controllers/application/SingleEntityHttpRequestController.ts
+++ packages/backend-http/src/http/controllers/application/SingleEntityHttpRequestController.ts
@@ -5,33 +5,26 @@ import { RequestWithBody } from '../../models/application/RequestWithBody';
 import { Response } from '../../models/application/Response';
 import { ResponseWithBody } from '../../models/application/ResponseWithBody';
 
-export class SingleEntityHttpRequestController<
+export abstract class SingleEntityHttpRequestController<
   TRequest extends Request | RequestWithBody = Request | RequestWithBody,
   TUseCaseParams extends unknown[] = unknown[],
   TModel = unknown,
-  TModelApi = unknown,
 > implements Handler<[TRequest], Response | ResponseWithBody<unknown>>
 {
   readonly #requestParamHandler: Handler<[TRequest], TUseCaseParams>;
-  readonly #useCaseHandler: Handler<TUseCaseParams, TModel | undefined>;
-  readonly #apiModelBuilder: Builder<TModelApi, [TModel]>;
   readonly #responseBuilder: Builder<
-    Response | ResponseWithBody<TModelApi>,
-    [TModelApi | undefined]
+    Response | ResponseWithBody<TModel>,
+    [TModel | undefined]
   >;
 
   constructor(
     requestParamHandler: Handler<[TRequest], TUseCaseParams>,
-    useCaseHandler: Handler<TUseCaseParams, TModel | undefined>,
-    apiModelBuilder: Builder<TModelApi, [TModel]>,
     responseBuilder: Builder<
-      Response | ResponseWithBody<TModelApi>,
-      [TModelApi | undefined]
+      Response | ResponseWithBody<TModel>,
+      [TModel | undefined]
     >,
   ) {
     this.#requestParamHandler = requestParamHandler;
-    this.#useCaseHandler = useCaseHandler;
-    this.#apiModelBuilder = apiModelBuilder;
     this.#responseBuilder = responseBuilder;
   }
 
@@ -40,21 +33,17 @@ export class SingleEntityHttpRequestController<
   ): Promise<Response | ResponseWithBody<unknown>> {
     const useCaseParams: TUseCaseParams =
       await this.#requestParamHandler.handle(request);
-    const model: TModel | undefined = await this.#useCaseHandler.handle(
+    const model: TModel | undefined = await this._handleUseCase(
       ...useCaseParams,
     );
 
-    let modelApi: TModelApi | undefined;
-
-    if (model === undefined) {
-      modelApi = undefined;
-    } else {
-      modelApi = this.#apiModelBuilder.build(model);
-    }
-
     const response: Response | ResponseWithBody<unknown> =
-      this.#responseBuilder.build(modelApi);
+      this.#responseBuilder.build(model);
 
     return response;
   }
+
+  protected abstract _handleUseCase(
+    ...useCaseParams: TUseCaseParams
+  ): Promise<TModel | undefined>;
 }
