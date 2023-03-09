b07c6a0f9c47c7fa35e1223fb77a52d265f86fe7
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-09 12:39:35 +0100

    refactor(backend-http): remove HttpController

--- packages/backend-http/src/index.ts
+++ packages/backend-http/src/index.ts
@@ -1,6 +1,5 @@
 import { SingleEntityGetResponseBuilder } from './http/builders/application/SingleEntityGetResponseBuilder';
 import { SingleEntityPostResponseBuilder } from './http/builders/application/SingleEntityPostResponseBuilder';
-import { HttpController } from './http/controllers/application/HttpController';
 import { SingleEntityHttpRequestController } from './http/controllers/application/SingleEntityHttpRequestController';
 import { Request } from './http/models/application/Request';
 import { RequestWithBody } from './http/models/application/RequestWithBody';
@@ -10,7 +9,6 @@ import { ResponseWithBody } from './http/models/application/ResponseWithBody';
 export type { Request, RequestWithBody, Response, ResponseWithBody };
 
 export {
-  HttpController,
   SingleEntityGetResponseBuilder,
   SingleEntityHttpRequestController,
   SingleEntityPostResponseBuilder,
