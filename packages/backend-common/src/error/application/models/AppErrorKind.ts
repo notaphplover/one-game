63f68b8a816e766a95ae4c6f4de2186b75c978de
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-09 21:13:02 +0100

    feature(backend-common): update AppErrorKind with entityConflict

--- packages/backend-common/src/error/application/models/AppErrorKind.ts
+++ packages/backend-common/src/error/application/models/AppErrorKind.ts
@@ -1,5 +1,6 @@
 export enum AppErrorKind {
   contractViolation = 'contractViolation',
+  entityConflict = 'entityConflict',
   unknown = 'unknown',
   unprocessableOperation = 'unprocessableOperation',
 }
