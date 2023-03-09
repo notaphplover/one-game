530941d33fb851bde9e5d61b5512015a727a3b61
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-09 12:44:41 +0100

    refactor(backend-service-user): add entities and migrationFolders

---
+++ packages/backend-service-user/src/foundation/db/adapter/nest/models/entities.ts
@@ -0,0 +1,4 @@
+import { UserDb } from '../../../../../user/adapter/typeorm/models/UserDb';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const typeOrmEntities: Function[] = [UserDb];
