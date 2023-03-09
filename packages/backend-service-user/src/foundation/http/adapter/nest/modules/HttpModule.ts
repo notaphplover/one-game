f61c5d60137ffc2dcc4f445d2eca426edd6e74a6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-09 14:11:40 +0100

    refactor(backend-service-user): update HttpModule with additional Builders

--- packages/backend-service-user/src/foundation/http/adapter/nest/modules/HttpModule.ts
+++ packages/backend-service-user/src/foundation/http/adapter/nest/modules/HttpModule.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SingleEntityPostResponseBuilder } from '@one-game-js/backend-http';
 
+import { RequestBuilder } from '../builders/RequestBuilder';
+import { RequestWithBodyBuilder } from '../builders/RequestWithBodyBuilder';
+import { ResponseBuilder } from '../builders/ResponseBuilder';
+
 @Module({
-  exports: [SingleEntityPostResponseBuilder],
-  providers: [SingleEntityPostResponseBuilder],
+  exports: [
+    RequestBuilder,
+    RequestWithBodyBuilder,
+    ResponseBuilder,
+    SingleEntityPostResponseBuilder,
+  ],
+  providers: [
+    RequestBuilder,
+    RequestWithBodyBuilder,
+    ResponseBuilder,
+    SingleEntityPostResponseBuilder,
+  ],
 })
 export class HttpModule {}
