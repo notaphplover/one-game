808a6f81d804d059862b72acc553b5fdde4a9705
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-14 01:33:48 +0100

    refactor(backend-service-user): update environment with jwt options

--- packages/backend/services/backend-service-user/src/foundation/env/application/services/EnvironmentLoader.ts
+++ packages/backend/services/backend-service-user/src/foundation/env/application/services/EnvironmentLoader.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { EnvLoader } from '@one-game-js/backend-env';
-import { cleanEnv, json, port } from 'envalid';
+import { JwtAlgorithm } from '@one-game-js/backend-jwt';
+import { cleanEnv, json, port, str, num } from 'envalid';
 
 import { Environment } from '../models/Environment';
 import { EnvironmentRaw } from '../models/EnvironmentRaw';
@@ -19,11 +20,25 @@ export class EnvironmentLoader extends EnvLoader<Environment> {
 
   protected _parseEnv(env: Record<string, string>): Environment {
     const rawEnvironment: EnvironmentRaw = cleanEnv(env, {
+      ONE_JS_USER_SERVICE_JWT_ALGORITHM: str<JwtAlgorithm>({
+        choices: Object.values(JwtAlgorithm),
+      }),
+      ONE_JS_USER_SERVICE_JWT_AUDIENCE: str(),
+      ONE_JS_USER_SERVICE_JWT_EXPIRATION_MS: num(),
+      ONE_JS_USER_SERVICE_JWT_ISSUER: str(),
+      ONE_JS_USER_SERVICE_JWT_PRIVATE_KEY: str(),
+      ONE_JS_USER_SERVICE_JWT_PUBLIC_KEY: str(),
       ONE_JS_USER_SERVICE_PORT: port(),
       ONE_JS_USER_SERVICE_TYPEORM_DATASOURCE_OPTIONS: json(),
     });
 
     return {
+      jwtAlgorithm: rawEnvironment.ONE_JS_USER_SERVICE_JWT_ALGORITHM,
+      jwtAudience: rawEnvironment.ONE_JS_USER_SERVICE_JWT_AUDIENCE,
+      jwtExpirationMs: rawEnvironment.ONE_JS_USER_SERVICE_JWT_EXPIRATION_MS,
+      jwtIssuer: rawEnvironment.ONE_JS_USER_SERVICE_JWT_ISSUER,
+      jwtPrivateKey: rawEnvironment.ONE_JS_USER_SERVICE_JWT_PRIVATE_KEY,
+      jwtPublicKey: rawEnvironment.ONE_JS_USER_SERVICE_JWT_PUBLIC_KEY,
       port: rawEnvironment.ONE_JS_USER_SERVICE_PORT,
       typeOrmDatasourceOptions:
         rawEnvironment.ONE_JS_USER_SERVICE_TYPEORM_DATASOURCE_OPTIONS,
