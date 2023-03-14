38e650e47dd0681c950bcc5565227fd083f3c986
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-14 20:21:52 +0100

    feature(backend-http): update SingleEntityHttpRequestController with optional middlewarePipeline

--- packages/backend/libraries/backend-http/src/http/application/controllers/SingleEntityHttpRequestController.ts
+++ packages/backend/libraries/backend-http/src/http/application/controllers/SingleEntityHttpRequestController.ts
@@ -4,6 +4,7 @@ import { Request } from '../models/Request';
 import { RequestWithBody } from '../models/RequestWithBody';
 import { Response } from '../models/Response';
 import { ResponseWithBody } from '../models/ResponseWithBody';
+import { MiddlewarePipeline } from '../modules/MiddlewarePipeline';
 
 export abstract class SingleEntityHttpRequestController<
   TRequest extends Request | RequestWithBody = Request | RequestWithBody,
@@ -20,6 +21,7 @@ export abstract class SingleEntityHttpRequestController<
     Response | ResponseWithBody<TModel>,
     [unknown]
   >;
+  readonly #middlewarePipeline: MiddlewarePipeline | undefined;
 
   constructor(
     requestParamHandler: Handler<[TRequest], TUseCaseParams>,
@@ -31,29 +33,41 @@ export abstract class SingleEntityHttpRequestController<
       Response | ResponseWithBody<TModel>,
       [unknown]
     >,
+    middlewarePipeline?: MiddlewarePipeline,
   ) {
     this.#requestParamHandler = requestParamHandler;
     this.#responseBuilder = responseBuilder;
     this.#responseFromErrorBuilder = responseFromErrorBuilder;
+    this.#middlewarePipeline = middlewarePipeline;
   }
 
   public async handle(
     request: TRequest,
   ): Promise<Response | ResponseWithBody<unknown>> {
     try {
-      const useCaseParams: TUseCaseParams =
-        await this.#requestParamHandler.handle(request);
-      const model: TModel | undefined = await this._handleUseCase(
-        ...useCaseParams,
-      );
-
       const response: Response | ResponseWithBody<unknown> =
-        this.#responseBuilder.build(model);
+        (await this.#middlewarePipeline?.apply(request)) ??
+        (await this.#handleUseCase(request));
 
       return response;
     } catch (error) {
       return this.#responseFromErrorBuilder.build(error);
     }
+  }
+
+  async #handleUseCase(
+    request: TRequest,
+  ): Promise<Response | ResponseWithBody<unknown>> {
+    const useCaseParams: TUseCaseParams =
+      await this.#requestParamHandler.handle(request);
+    const model: TModel | undefined = await this._handleUseCase(
+      ...useCaseParams,
+    );
+
+    const response: Response | ResponseWithBody<unknown> =
+      this.#responseBuilder.build(model);
+
+    return response;
   }
 
   protected abstract _handleUseCase(
