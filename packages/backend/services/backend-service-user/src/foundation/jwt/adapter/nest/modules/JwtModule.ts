75e07aaebe565a30fb4fa88aa5310d980cf57418
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-14 01:35:49 +0100

    refactor(backend-service-user): add JwtModule

---
+++ packages/backend/services/backend-service-user/src/foundation/jwt/adapter/nest/modules/JwtModule.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { JwtService } from '@one-game-js/backend-jwt';
+
+import { UserJwtPayload } from '../../../../../user/application/models/UserJwtPayload';
+import { EnvModule } from '../../../../env/adapter/nest/modules/EnvModule';
+import { Environment } from '../../../../env/application/models/Environment';
+import { EnvironmentService } from '../../../../env/application/services/EnvironmentService';
+
+@Module({
+  imports: [EnvModule],
+  providers: [
+    {
+      provide: JwtService,
+      useFactory: (
+        environmentService: EnvironmentService,
+      ): JwtService<UserJwtPayload> => {
+        const env: Environment = environmentService.getEnvironment();
+
+        return new JwtService({
+          algorithm: env.jwtAlgorithm,
+          audience: env.jwtAudience,
+          expirationMs: env.jwtExpirationMs,
+          issuer: env.jwtIssuer,
+          privateKey: env.jwtPrivateKey,
+          publicKey: env.jwtPublicKey,
+        });
+      },
+    },
+  ],
+})
+export class JwtModule {}
