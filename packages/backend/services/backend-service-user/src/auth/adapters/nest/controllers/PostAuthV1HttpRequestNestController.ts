8745055692910f5f784ef8f7b2cf480be40b339b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-16 00:33:54 +0100

    refactor(backend-service-user): add PostAuthV1HttpRequestNestController

---
+++ packages/backend/services/backend-service-user/src/auth/adapters/nest/controllers/PostAuthV1HttpRequestNestController.ts
@@ -0,0 +1,41 @@
+import { Controller, Inject, Post, Req, Res } from '@nestjs/common';
+import { Builder, Handler } from '@one-game-js/backend-common';
+import {
+  RequestWithBodyFromFastifyRequestBuilder,
+  FastifyReplyFromResponseBuilder,
+  HttpNestFastifyController,
+  RequestWithBody,
+  Response,
+  ResponseWithBody,
+} from '@one-game-js/backend-http';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { PostAuthV1HttpRequestController } from '../../../application/controllers/PostAuthV1HttpRequestController';
+
+@Controller('v1/auth')
+export class PostAuthV1HttpRequestNestController extends HttpNestFastifyController<RequestWithBody> {
+  constructor(
+    @Inject(RequestWithBodyFromFastifyRequestBuilder)
+    requestBuilder: Builder<RequestWithBody, [FastifyRequest]>,
+    @Inject(PostAuthV1HttpRequestController)
+    requestController: Handler<
+      [RequestWithBody],
+      Response | ResponseWithBody<unknown>
+    >,
+    @Inject(FastifyReplyFromResponseBuilder)
+    resultBuilder: Builder<
+      FastifyReply,
+      [Response | ResponseWithBody<unknown>, FastifyReply]
+    >,
+  ) {
+    super(requestBuilder, requestController, resultBuilder);
+  }
+
+  @Post()
+  public override async handle(
+    @Req() request: FastifyRequest,
+    @Res() reply: FastifyReply,
+  ): Promise<void> {
+    await super.handle(request, reply);
+  }
+}
