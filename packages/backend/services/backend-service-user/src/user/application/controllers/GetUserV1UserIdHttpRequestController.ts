5416ffaba5cd6c97dd47dd74da4b22a51bb83050
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-03-17 00:36:05 +0100

    refactor(backend-service-user): add GetUserV1UserIdHttpRequestController

---
+++ packages/backend/services/backend-service-user/src/user/application/controllers/GetUserV1UserIdHttpRequestController.ts
@@ -0,0 +1,56 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { models as apiModels } from '@one-game-js/api-models';
+import { Builder, Handler } from '@one-game-js/backend-common';
+import {
+  ErrorV1ResponseFromErrorBuilder,
+  MiddlewarePipeline,
+  Request,
+  Response,
+  ResponseWithBody,
+  SingleEntityGetResponseBuilder,
+  SingleEntityHttpRequestController,
+} from '@one-game-js/backend-http';
+
+import { AuthMiddleware } from '../../../auth/application/middlewares/AuthMiddleware';
+import { GetUserV1UserIdRequestParamHandler } from '../handlers/GetUserV1UserIdRequestParamHandler';
+import { UserManagementInputPort } from '../ports/input/UserManagementInputPort';
+
+@Injectable()
+export class GetUserV1UserIdHttpRequestController extends SingleEntityHttpRequestController<
+  Request,
+  [string],
+  apiModels.UserV1
+> {
+  readonly #userManagementInputPort: UserManagementInputPort;
+
+  constructor(
+    @Inject(GetUserV1UserIdRequestParamHandler)
+    requestParamHandler: Handler<[Request], [string]>,
+    @Inject(SingleEntityGetResponseBuilder)
+    responseBuilder: Builder<Response, [apiModels.UserV1 | undefined]>,
+    @Inject(ErrorV1ResponseFromErrorBuilder)
+    errorV1ResponseFromErrorBuilder: Builder<
+      Response | ResponseWithBody<unknown>,
+      [unknown]
+    >,
+    @Inject(UserManagementInputPort)
+    userManagementInputPort: UserManagementInputPort,
+    @Inject(AuthMiddleware)
+    authMiddleware: AuthMiddleware,
+  ) {
+    super(
+      requestParamHandler,
+      responseBuilder,
+      errorV1ResponseFromErrorBuilder,
+      new MiddlewarePipeline([authMiddleware]),
+    );
+
+    this.#userManagementInputPort = userManagementInputPort;
+  }
+
+  protected async _handleUseCase(
+    userId: string,
+  ): Promise<apiModels.UserV1 | undefined> {
+    return this.#userManagementInputPort.findOne(userId);
+  }
+}
