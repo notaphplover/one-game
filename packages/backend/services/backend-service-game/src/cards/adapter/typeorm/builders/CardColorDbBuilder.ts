bceb0264bb6d2fc10682d030dccf6e59b9052b06
notaphplover <roberto.pintos.lopez@gmail.com>
2023-03-19 12:43:12 +0100

    refactor(backend-service-game): add CardColorDbBuilder

---
+++ packages/backend/services/backend-service-game/src/cards/adapter/typeorm/builders/CardColorDbBuilder.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Builder } from '@one-game-js/backend-common';
+
+import { CardColor } from '../../../domain/models/CardColor';
+import { CardColorDb } from '../models/CardColorDb';
+import {
+  CARD_V1_MASK,
+  COLORED_BLUE_SUBTYPE_MASK,
+  COLORED_GREEN_SUBTYPE_MASK,
+  COLORED_RED_SUBTYPE_MASK,
+  COLORED_YELLOW_SUBTYPE_MASK,
+} from '../models/cardDbMasks';
+
+@Injectable()
+export class CardColorDbBuilder implements Builder<CardColorDb, [CardColor]> {
+  public build(cardColor: CardColor): CardColorDb {
+    let cardColorDb: CardColorDb = CARD_V1_MASK;
+
+    switch (cardColor) {
+      case CardColor.blue:
+        cardColorDb |= COLORED_BLUE_SUBTYPE_MASK;
+        break;
+      case CardColor.green:
+        cardColorDb |= COLORED_GREEN_SUBTYPE_MASK;
+        break;
+      case CardColor.red:
+        cardColorDb |= COLORED_RED_SUBTYPE_MASK;
+        break;
+      case CardColor.yellow:
+        cardColorDb |= COLORED_YELLOW_SUBTYPE_MASK;
+        break;
+    }
+
+    return cardColorDb;
+  }
+}
