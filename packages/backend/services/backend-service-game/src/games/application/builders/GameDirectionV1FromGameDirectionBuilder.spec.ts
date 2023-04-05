e453463ef4f2af3f66258a8d7d3774e9940c2eae
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-04-05 19:00:23 +0200

    refactor(backend-service-game): add GameDirectionV1FromGameDirectionBuilder

---
+++ packages/backend/services/backend-service-game/src/games/application/builders/GameDirectionV1FromGameDirectionBuilder.spec.ts
@@ -0,0 +1,41 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { models as apiModels } from '@one-game-js/api-models';
+
+import { GameDirection } from '../../domain/models/GameDirection';
+import { GameDirectionV1FromGameDirectionBuilder } from './GameDirectionV1FromGameDirectionBuilder';
+
+describe(GameDirectionV1FromGameDirectionBuilder.name, () => {
+  let gameDirectionToGameDirectionDbConverter: GameDirectionV1FromGameDirectionBuilder;
+
+  beforeAll(() => {
+    gameDirectionToGameDirectionDbConverter =
+      new GameDirectionV1FromGameDirectionBuilder();
+  });
+
+  describe('.convert', () => {
+    describe.each<[GameDirection, apiModels.GameDirectionV1]>([
+      [GameDirection.antiClockwise, 'antiClockwise'],
+      [GameDirection.clockwise, 'clockwise'],
+    ])(
+      'having a game direction %s',
+      (
+        gameDirection: GameDirection,
+        gameDirectionV1: apiModels.GameDirectionV1,
+      ) => {
+        describe('when called', () => {
+          let result: unknown;
+
+          beforeAll(() => {
+            result =
+              gameDirectionToGameDirectionDbConverter.build(gameDirection);
+          });
+
+          it('should return a GameDirectionDb', () => {
+            expect(result).toBe(gameDirectionV1);
+          });
+        });
+      },
+    );
+  });
+});
