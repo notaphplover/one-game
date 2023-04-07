c2f242062d9c2bb1a68f172b8faab974b497ddb3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-04-07 12:47:13 +0200

    refactor(backend-service-game): add GameHttpApiModule

---
+++ packages/backend/services/backend-service-game/src/games/adapter/nest/modules/GameHttpApiModule.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+
+import { HttpModule } from '../../../../foundation/http/adapter/nest/modules/HttpModule';
+import { JsonSchemaModule } from '../../../../foundation/jsonSchema/adapter/nest/modules/JsonSchemaModule';
+import { PostGameV1HttpRequestController } from '../../../application/controllers/PostGameV1HttpRequestController';
+import { PostGameV1RequestParamHandler } from '../../../application/handlers/PostGameV1RequestParamHandler';
+import { PostGameV1HttpRequestNestController } from '../controllers/PostGameV1HttpRequestNestController';
+import { GameModule } from './GameModule';
+
+@Module({
+  controllers: [PostGameV1HttpRequestNestController],
+  imports: [JsonSchemaModule, HttpModule, GameModule],
+  providers: [
+    PostGameV1HttpRequestController,
+    PostGameV1HttpRequestNestController,
+    PostGameV1RequestParamHandler,
+  ],
+})
+export class GameHttpApiModule {}
