aaaa6ca10b2c68a135ff428c9850d878e742d126
notaphplover <roberto.pintos.lopez@gmail.com>
2023-04-19 02:15:07 +0200

    refactor(backend-service-game): update GamePersistenceOutputPort with findOne

--- packages/backend/services/backend-service-game/src/games/adapter/typeorm/adapters/GamePersistenceTypeOrmAdapter.ts
+++ packages/backend/services/backend-service-game/src/games/adapter/typeorm/adapters/GamePersistenceTypeOrmAdapter.ts
@@ -3,22 +3,34 @@ import { Inject, Injectable } from '@nestjs/common';
 import { GamePersistenceOutputPort } from '../../../application/ports/output/GamePersistenceOutputPort';
 import { Game } from '../../../domain/models/Game';
 import { GameCreateQuery } from '../../../domain/query/GameCreateQuery';
+import { GameFindQuery } from '../../../domain/query/GameFindQuery';
 import { CreateGameTypeOrmService } from '../services/CreateGameTypeOrmService';
+import { FindGameTypeOrmService } from '../services/FindGameTypeOrmService';
 
 @Injectable()
 export class GamePersistenceTypeOrmAdapter
   implements GamePersistenceOutputPort
 {
   readonly #createGameTypeOrmService: CreateGameTypeOrmService;
+  readonly #findGameTypeOrmService: FindGameTypeOrmService;
 
   constructor(
     @Inject(CreateGameTypeOrmService)
     createGameTypeOrmService: CreateGameTypeOrmService,
+    @Inject(FindGameTypeOrmService)
+    findGameTypeOrmService: FindGameTypeOrmService,
   ) {
     this.#createGameTypeOrmService = createGameTypeOrmService;
+    this.#findGameTypeOrmService = findGameTypeOrmService;
   }
 
   public async create(gameCreateQuery: GameCreateQuery): Promise<Game> {
     return this.#createGameTypeOrmService.insertOne(gameCreateQuery);
+  }
+
+  public async findOne(
+    gameFindQuery: GameFindQuery,
+  ): Promise<Game | undefined> {
+    return this.#findGameTypeOrmService.findOne(gameFindQuery);
   }
 }
--- packages/backend/services/backend-service-game/src/games/application/ports/output/GamePersistenceOutputPort.ts
+++ packages/backend/services/backend-service-game/src/games/application/ports/output/GamePersistenceOutputPort.ts
@@ -1,8 +1,10 @@
 import { Game } from '../../../domain/models/Game';
 import { GameCreateQuery } from '../../../domain/query/GameCreateQuery';
+import { GameFindQuery } from '../../../domain/query/GameFindQuery';
 
 export interface GamePersistenceOutputPort {
   create(gameCreateQuery: GameCreateQuery): Promise<Game>;
+  findOne(gameFindQuery: GameFindQuery): Promise<Game | undefined>;
 }
 
 export const gamePersistenceOutputPortSymbol: symbol = Symbol.for(
