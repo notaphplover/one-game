9c0bc031be42f0c32983502bd5ae4b3a873ac0cf
Mario <mario.fdez.p@gmail.com>
2023-04-30 12:29:19 +0200

    refactor: rename login component

--- packages/frontend/web-ui/src/main.jsx
+++ packages/frontend/web-ui/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Login } from './components/Login';
 import App from './App';
-import { FormCustomHook } from './CustomHook/FormCustomHook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormCustomHook />
+    <Login />
   </React.StrictMode>,
 )
