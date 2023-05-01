634a756220e60a1d86dc9fe3d611fdf879b123c3
notaphplover <roberto.pintos.lopez@gmail.com>
2023-05-01 18:09:21 +0200

    refactor(backend-service-game): add GameCanHoldOnlyOneMoreGameSlotSpec

---
+++ packages/backend/services/backend-service-game/src/games/domain/specs/GameCanHoldOnlyOneMoreGameSlotSpec.spec.ts
@@ -0,0 +1,43 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { ActiveGameFixtures } from '../fixtures/ActiveGameFixtures';
+import { NonStartedGameFixtures } from '../fixtures/NonStartedGameFixtures';
+import { Game } from '../models/Game';
+import { GameCanHoldOnlyOneMoreGameSlotSpec } from './GameCanHoldOnlyOneMoreGameSlotSpec';
+
+describe(GameCanHoldOnlyOneMoreGameSlotSpec.name, () => {
+  let gameCanHoldOnlyOneMoreGameSlotSpec: GameCanHoldOnlyOneMoreGameSlotSpec;
+
+  beforeAll(() => {
+    gameCanHoldOnlyOneMoreGameSlotSpec =
+      new GameCanHoldOnlyOneMoreGameSlotSpec();
+  });
+
+  describe('.isSatisfiedBy', () => {
+    describe.each<[string, Game, boolean]>([
+      [
+        'a NonStartedGame with one free slot',
+        NonStartedGameFixtures.withGameSlotsAmountOneAndSlotsZero,
+        true,
+      ],
+      [
+        'a NonStartedGame with no free slots',
+        NonStartedGameFixtures.withGameSlotsAmountOneAndSlotsOne,
+        false,
+      ],
+      ['an ActiveGame', ActiveGameFixtures.any, false],
+    ])('having %s', (_: string, game: Game, expectedResult: boolean) => {
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = gameCanHoldOnlyOneMoreGameSlotSpec.isSatisfiedBy(game);
+        });
+
+        it(`should return ${expectedResult.toString()}`, () => {
+          expect(result).toBe(expectedResult);
+        });
+      });
+    });
+  });
+});
