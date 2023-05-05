0acd3a241adc6d96d86c828109ead6d95d9c5900
Mario <mario.fdez.p@gmail.com>
2023-05-05 20:26:35 +0200

    chore(web-ui): update vite config with additional commonJs packages

--- packages/frontend/web-ui/vite.config.js
+++ packages/frontend/web-ui/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: ['@cornie-js/api-http-client', /node_modules/],
+    },
+  },
+  optimizeDeps: {
+    include: ['@cornie-js/api-http-client']
+  },
   plugins: [react()],
 })
