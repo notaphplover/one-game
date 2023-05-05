a242402a4bc9a55ec782043b2461c75a13c181e2
Mario <mario.fdez.p@gmail.com>
2023-05-05 16:28:45 +0200

    refactor(web-ui): update app component to include login one

--- packages/frontend/web-ui/src/main.jsx
+++ packages/frontend/web-ui/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Login } from './components/Login';
-import App from './App';
+import CornieApp from './CornieApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <CornieApp />
   </React.StrictMode>,
 )
