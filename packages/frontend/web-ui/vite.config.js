811e14f63cd89958cb5456c8f43c17c580c483de
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-05-06 13:52:35 +0200

    chore(web-ui): update vite config to avoid unnecesary complexity

--- packages/frontend/web-ui/vite.config.js
+++ packages/frontend/web-ui/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    commonjsOptions: {
-      include: ['@cornie-js/api-http-client', /node_modules/],
-    },
-  },
-  optimizeDeps: {
-    include: ['@cornie-js/api-http-client']
-  },
   plugins: [react()],
-})
+});
