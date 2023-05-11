08d35a6916186bbba3c52073cd1998d493cb1731
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-05-11 21:52:51 +0200

    chore(backend-app-user-models): expose userPersistenceOutputPortSymbol

--- packages/backend/libraries/backend-app-user-models/src/index.ts
+++ packages/backend/libraries/backend-app-user-models/src/index.ts
@@ -1,8 +1,13 @@
 import { UserJwtPayload } from './user/application/models/UserJwtPayload';
-import { UserPersistenceOutputPort } from './user/application/port/output/UserPersistenceOutputPort';
+import {
+  UserPersistenceOutputPort,
+  userPersistenceOutputPortSymbol,
+} from './user/application/port/output/UserPersistenceOutputPort';
 import { User } from './user/domain/models/User';
 import { UserCreateQuery } from './user/domain/models/UserCreateQuery';
 import { UserFindQuery } from './user/domain/models/UserFindQuery';
+
+export { userPersistenceOutputPortSymbol };
 
 export type {
   UserJwtPayload,
