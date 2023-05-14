d0b7a474997b9097b6ed126b0deaaa2dffe8fa79
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-05-14 11:47:56 +0200

    feat(backend-app-user): update seed script to rely on environment

--- packages/backend/apps/user/backend-app-user/src/app/adapter/nest/scripts/seedDatabase.ts
+++ packages/backend/apps/user/backend-app-user/src/app/adapter/nest/scripts/seedDatabase.ts
@@ -1,21 +1,46 @@
+import { EnvModule, EnvironmentService } from '@cornie-js/backend-app-user-env';
 import {
   PipelineSeeder,
   SeederModule,
 } from '@cornie-js/backend-app-user-seeder';
-import { INestApplicationContext } from '@nestjs/common';
+import { DynamicModule, INestApplicationContext, Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { buildDbModuleOptions } from '../../../../foundation/db/adapter/nest/calculations/buildDbModuleOptions';
 
+@Module({})
+class SeedDatabaseModule {
+  public static forRoot(seederModule: DynamicModule): DynamicModule {
+    return {
+      exports: [EnvModule, seederModule],
+      global: false,
+      imports: [EnvModule, seederModule],
+      module: SeedDatabaseModule,
+    };
+  }
+}
+
 export async function seedDatabase(): Promise<void> {
+  const seederModule: DynamicModule = SeederModule.forRootAsync(
+    buildDbModuleOptions(),
+  );
+
   const nestAppContext: INestApplicationContext =
     await NestFactory.createApplicationContext(
-      SeederModule.forRootAsync(buildDbModuleOptions()),
+      SeedDatabaseModule.forRoot(seederModule),
     );
+
+  const environmentService: EnvironmentService =
+    nestAppContext.get(EnvironmentService);
+
+  const seedDummyData: boolean =
+    environmentService.getEnvironment().seedDummyData;
 
   const pipelineSeeder: PipelineSeeder = nestAppContext.get(PipelineSeeder);
 
-  await pipelineSeeder.seed();
+  await pipelineSeeder.seed({
+    seedDummy: seedDummyData,
+  });
 
   await nestAppContext.close();
 }
