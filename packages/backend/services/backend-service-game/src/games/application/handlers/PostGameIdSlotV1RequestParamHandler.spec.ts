bfaffb114062697095ca03fe9d77c1228b11406f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-05-31 20:09:59 +0200

    fix(backend-service-game): update PostGameIdSlotV1HttpRequestController to avoid users impersonating other ones

--- packages/backend/services/backend-service-game/src/games/application/handlers/PostGameIdSlotV1RequestParamHandler.spec.ts
+++ packages/backend/services/backend-service-game/src/games/application/handlers/PostGameIdSlotV1RequestParamHandler.spec.ts
@@ -3,9 +3,12 @@ import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
 import { models as apiModels } from '@cornie-js/api-models';
 import { GameSlotCreateV1Fixtures } from '@cornie-js/backend-app-game-fixtures/games/application';
 import { NonStartedGameFixtures } from '@cornie-js/backend-app-game-fixtures/games/domain';
+import { UserV1Fixtures } from '@cornie-js/backend-app-game-fixtures/users/application';
 import { Game } from '@cornie-js/backend-app-game-models/games/domain';
-import { Handler } from '@cornie-js/backend-common';
+import { AppError, AppErrorKind, Handler } from '@cornie-js/backend-common';
 import {
+  AuthKind,
+  AuthRequestContextHolder,
   RequestWithBody,
   requestContextProperty,
 } from '@cornie-js/backend-http';
@@ -32,53 +35,122 @@ describe(PostGameIdSlotV1RequestParamHandler.name, () => {
   });
 
   describe('.handle', () => {
-    let requestFixture: RequestWithBody & GameRequestContextHolder;
+    describe('having a request with backend auth', () => {
+      let requestFixture: RequestWithBody &
+        AuthRequestContextHolder &
+        GameRequestContextHolder;
 
-    describe('when called', () => {
-      let gameIdSlotCreateQueryV1Fixture: apiModels.GameIdSlotCreateQueryV1;
-
-      let result: unknown;
-
-      beforeAll(async () => {
+      beforeAll(() => {
         requestFixture = {
           [requestContextProperty]: {
+            auth: {
+              kind: AuthKind.backendService,
+            },
             game: NonStartedGameFixtures.any,
           },
         } as Partial<
-          RequestWithBody & GameRequestContextHolder
-        > as RequestWithBody & GameRequestContextHolder;
-
-        gameIdSlotCreateQueryV1Fixture = GameSlotCreateV1Fixtures.any;
-
-        postGameIdSlotV1RequestBodyHandlerMock.handle.mockResolvedValueOnce([
-          gameIdSlotCreateQueryV1Fixture,
-        ]);
-
-        result = await postGameIdSlotV1RequestParamHandler.handle(
-          requestFixture,
-        );
+          RequestWithBody & AuthRequestContextHolder & GameRequestContextHolder
+        > as RequestWithBody &
+          AuthRequestContextHolder &
+          GameRequestContextHolder;
       });
 
-      afterAll(() => {
-        jest.clearAllMocks();
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(async () => {
+          try {
+            await postGameIdSlotV1RequestParamHandler.handle(requestFixture);
+          } catch (error) {
+            result = error;
+          }
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should throw an Error', () => {
+          const expectedErrorProperties: Partial<AppError> = {
+            kind: AppErrorKind.invalidCredentials,
+            message: 'Expecting user based credentials',
+          };
+
+          expect(result).toBeInstanceOf(AppError);
+          expect(result).toStrictEqual(
+            expect.objectContaining(expectedErrorProperties),
+          );
+        });
+      });
+    });
+
+    describe('having a request with user auth', () => {
+      let userFixture: apiModels.UserV1;
+      let requestFixture: RequestWithBody &
+        AuthRequestContextHolder &
+        GameRequestContextHolder;
+
+      beforeAll(() => {
+        userFixture = UserV1Fixtures.any;
+
+        requestFixture = {
+          [requestContextProperty]: {
+            auth: {
+              kind: AuthKind.user,
+              user: userFixture,
+            },
+            game: NonStartedGameFixtures.any,
+          },
+        } as Partial<
+          RequestWithBody & AuthRequestContextHolder & GameRequestContextHolder
+        > as RequestWithBody &
+          AuthRequestContextHolder &
+          GameRequestContextHolder;
       });
 
-      it('should call postGameIdSlotV1RequestBodyHandlerMock.handle()', () => {
-        expect(
-          postGameIdSlotV1RequestBodyHandlerMock.handle,
-        ).toHaveBeenCalledTimes(1);
-        expect(
-          postGameIdSlotV1RequestBodyHandlerMock.handle,
-        ).toHaveBeenCalledWith(requestFixture);
-      });
+      describe('when called', () => {
+        let gameIdSlotCreateQueryV1Fixture: apiModels.GameIdSlotCreateQueryV1;
 
-      it('should return an array', () => {
-        const expected: [apiModels.GameIdSlotCreateQueryV1, Game] = [
-          gameIdSlotCreateQueryV1Fixture,
-          requestFixture[requestContextProperty].game,
-        ];
+        let result: unknown;
 
-        expect(result).toStrictEqual(expected);
+        beforeAll(async () => {
+          gameIdSlotCreateQueryV1Fixture = GameSlotCreateV1Fixtures.any;
+
+          postGameIdSlotV1RequestBodyHandlerMock.handle.mockResolvedValueOnce([
+            gameIdSlotCreateQueryV1Fixture,
+          ]);
+
+          result = await postGameIdSlotV1RequestParamHandler.handle(
+            requestFixture,
+          );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call postGameIdSlotV1RequestBodyHandlerMock.handle()', () => {
+          expect(
+            postGameIdSlotV1RequestBodyHandlerMock.handle,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            postGameIdSlotV1RequestBodyHandlerMock.handle,
+          ).toHaveBeenCalledWith(requestFixture);
+        });
+
+        it('should return an array', () => {
+          const expected: [
+            apiModels.GameIdSlotCreateQueryV1,
+            Game,
+            apiModels.UserV1,
+          ] = [
+            gameIdSlotCreateQueryV1Fixture,
+            requestFixture[requestContextProperty].game,
+            userFixture,
+          ];
+
+          expect(result).toStrictEqual(expected);
+        });
       });
     });
   });
