541c119c8bf5d428b4f652e18b402441d95a750e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-06-02 21:14:49 +0200

    chore(backend-test-e2e): add cucumber config

---
+++ packages/backend/tools/backend-test-e2e/config/cucumber.js.config.mjs
@@ -0,0 +1,28 @@
+import { getBaseConfiguration } from './cucumber.config.base.mjs';
+
+/**
+ * @param {boolean} parallel
+ * @returns {!import("@cucumber/cucumber/lib/configuration").IConfiguration}
+ */
+function getConfiguration(parallel) {
+  /** @type {!import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+  const config = {
+    ...getBaseConfiguration(parallel),
+    require: [
+      'lib/*/step-definitions/*.js',
+      'lib/app/adapter/cucumber/hooks/*.js',
+    ],
+  };
+
+  return config;
+}
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const serial = getConfiguration(false);
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const parallel = getConfiguration(true);
+
+export default serial;
+
+export { serial, parallel };
---
+++ packages/backend/tools/backend-test-e2e/config/cucumber.ts.config.mjs
@@ -0,0 +1,29 @@
+import { getBaseConfiguration } from './cucumber.config.base.mjs';
+
+/**
+ * @param {boolean} parallel
+ * @returns {!import("@cucumber/cucumber/lib/configuration").IConfiguration}
+ */
+function getConfiguration(parallel) {
+  /** @type {!import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+  const config = {
+    ...getBaseConfiguration(parallel),
+    require: [
+      'src/*/step-definitions/*.ts',
+      'src/app/adapter/cucumber/hooks/*.ts',
+    ],
+    requireModule: ['tsconfig-paths/register', 'ts-node/register'],
+  };
+
+  return config;
+}
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const serial = getConfiguration(false);
+
+/** @type {import("@cucumber/cucumber/lib/configuration").IConfiguration} */
+const parallel = getConfiguration(true);
+
+export default serial;
+
+export { serial, parallel };
