da2163c497fe20f209dbfce899e6f51385b44680
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-06-02 21:20:45 +0200

    chore(backend-test-e2e): add global hooks

---
+++ packages/backend/tools/backend-test-e2e/src/app/adapter/cucumber/hooks/before.ts
@@ -0,0 +1,17 @@
+import { Before } from '@cucumber/cucumber';
+import { INestApplicationContext } from '@nestjs/common';
+
+import { EnvironmentService } from '../../../../env/application/services/EnvironmentService';
+import { OneGameApiWorld } from '../../../../http/models/OneGameApiWorld';
+import { initializeWorld } from '../../../../http/utils/actions/initializeWorld';
+import { applicationContext } from '../../nest/contexts/applicationContext';
+
+Before<Partial<OneGameApiWorld>>(async function () {
+  const resolvedApplicationContext: INestApplicationContext =
+    await applicationContext;
+
+  const environmentService: EnvironmentService =
+    resolvedApplicationContext.get(EnvironmentService);
+
+  initializeWorld(this, environmentService.getEnvironment().apiBackendBaseUrl);
+});
