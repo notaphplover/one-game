7e3b90527fef485f7674e2fa1a9bdefcd7fa829e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-06-21 18:32:30 +0200

    refactor(backend-service-game): update backend-service-game package to rely on backend-app-game-db

--- packages/backend/apps/game/backend-app-game-db/src/index.ts
+++ packages/backend/apps/game/backend-app-game-db/src/index.ts
@@ -1,8 +1,9 @@
 import { provideTypeOrmDataSource } from './app/adapter/typeorm/scripts/provideTypeOrmDatasource';
+import { runMigrations } from './app/adapter/typeorm/scripts/runMigrations';
 import { DbModuleOptions } from './foundation/db/adapter/nest/models/DbModuleOptions';
 import { DbModule } from './foundation/db/adapter/nest/modules/DbModule';
 import { GameDbModule } from './games/adapter/nest/modules/GameDbModule';
 
 export type { DbModuleOptions };
 
-export { DbModule, provideTypeOrmDataSource, GameDbModule };
+export { DbModule, provideTypeOrmDataSource, GameDbModule, runMigrations };
