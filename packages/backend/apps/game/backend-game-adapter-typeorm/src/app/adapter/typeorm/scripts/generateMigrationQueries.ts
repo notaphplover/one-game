9992fe3e9624fb2b9c2a8641585554d134e1a48c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-06-23 18:15:42 +0200

    chore(backend-game-adapter-typeorm): add generateMigrationQueries

---
+++ packages/backend/apps/game/backend-game-adapter-typeorm/src/app/adapter/typeorm/scripts/generateMigrationQueries.ts
@@ -0,0 +1,45 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SqlInMemory } from 'typeorm/driver/SqlInMemory.js';
+
+import { provideTypeOrmDataSource } from './provideTypeOrmDatasource';
+
+export async function generateMigrationQueries(
+  dataSourceOptions: DataSourceOptions,
+): Promise<void> {
+  const datasource: DataSource = await provideTypeOrmDataSource(
+    dataSourceOptions,
+  );
+
+  datasource.setOptions({
+    dropSchema: false,
+    logging: false,
+    migrationsRun: false,
+    synchronize: false,
+  });
+
+  await datasource.initialize();
+
+  try {
+    const { upQueries, downQueries }: SqlInMemory = await datasource.driver
+      .createSchemaBuilder()
+      .log();
+
+    console.log('Up queries:');
+    console.log();
+
+    for (const query of upQueries) {
+      console.log(query.query);
+    }
+    console.log();
+
+    console.log('Down queries:');
+    console.log();
+
+    for (const query of downQueries) {
+      console.log(query.query);
+    }
+    console.log();
+  } finally {
+    await datasource.destroy();
+  }
+}
