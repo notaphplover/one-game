b542057812b369d8a7b2d74e3dae02c2e94f464d
Mario <mario.fdez.p@gmail.com>
2023-06-29 20:03:03 +0200

    feat(web-ui): update login page appearance

--- packages/frontend/web-ui/src/auth/pages/Register.jsx
+++ packages/frontend/web-ui/src/auth/pages/Register.jsx
@@ -3,9 +3,9 @@ import { Link as RouterLink } from 'react-router-dom';
 import { useForm } from '../../cornie/hooks/useForm';
 import { AuthLayout } from '../layout/AuthLayout';
 
-export const Login = () => {
+export const Register = () => {
 
-    const {email, password, onInputChange} = useForm({
+    const {formState, displayName, email, password, onInputChange, onResetForm} = useForm({
         username: '',
         email: '',
         password: ''
@@ -13,11 +13,24 @@ export const Login = () => {
 
     return (
 
-        <AuthLayout title="Welcome to Cornie's game !!">
+        <AuthLayout title="Create a Cornie's account">
 
         <form>
           
           <Grid container>
+          <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Alias" 
+                type="text" 
+                placeholder="alias" 
+                fullWidth 
+                name="alias"
+                value={displayName}
+                onChange={onInputChange}
+                //error={emailValid !== null}
+                //helperText={emailValid}
+              />
+            </Grid>
             <Grid item xs={12} sx={{mt: 2}}>
               <TextField 
                 label="e-mail" 
@@ -44,11 +57,24 @@ export const Login = () => {
                 //helperText={passwordValid}
               />
             </Grid>
-          </Grid>
-          
+            <Grid item xs={12} sx={{mt: 2}}>
+                <TextField 
+                  label="Confirm Password" 
+                  type="password" 
+                  placeholder="*******" 
+                  fullWidth 
+                  name="confirmPassword"
+                  value={password}
+                  onChange={onInputChange}
+                  //error={passwordValid !== null}
+                  //helperText={passwordValid}
+                />
+              </Grid>
+            </Grid>
+
           <Grid container spacing={2} sx={{mt: 2, mb: 2}}>
             
-            <Grid item xs={12}>
+            <Grid item xs={6} sm={12}>
               <Button 
                 //disabled={isAuthenticating}
                 type="submit" 
@@ -56,14 +82,19 @@ export const Login = () => {
                 fullWidth
                 //onClick={onSubmit}
               >
-                <Typography textAlign='center'>Login</Typography> 
+                <Typography textAlign='center'>Create</Typography> 
               </Button>
             </Grid>  
 
-            <Grid container direction='row' justifyContent='end' sx={{mt: 4}}>
-              <Typography sx={{mt: 2, mr:1}}> {`Don't you have a Cornie's account?`}</Typography>
-              <Link sx={{mt: 2, mr:1}} component={RouterLink} color='primary' to="/auth/register">
-                Sign up
+            <Grid 
+              container 
+              direction='row' 
+              justifyContent='end' 
+              sx={{mt: 4}}
+            >
+              <Typography sx={{mt: 2, mr:1}}> Do you have a Cornie's account? </Typography>
+              <Link sx={{mt: 2, mr:1}} component={RouterLink} color='inherit' to="/auth/login">
+                Sign in
               </Link>
             </Grid>
 
