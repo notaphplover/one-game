8f1c2464799a674f1c904a42a870f71818fcaf47
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-03 21:27:05 +0200

    feat(backend-service-game): add PatchGameV1GameIdHttpRequestNestController

---
+++ packages/backend/services/backend-service-game/src/games/adapter/nest/controllers/PatchGameV1GameIdHttpRequestNestController.ts
@@ -0,0 +1,45 @@
+import { Builder, Handler } from '@cornie-js/backend-common';
+import {
+  PatchGameV1GameIdHttpRequestController,
+  PatchGameGameIdV1RequestParamHandler,
+} from '@cornie-js/backend-game-application/games';
+import {
+  FastifyReplyFromResponseBuilder,
+  HttpNestFastifyController,
+  RequestWithBody,
+  RequestWithBodyFromFastifyRequestBuilder,
+  Response,
+  ResponseWithBody,
+} from '@cornie-js/backend-http';
+import { Controller, Inject, Patch, Req, Res } from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+@Controller('v1/games')
+export class PatchGameV1GameIdHttpRequestNestController extends HttpNestFastifyController<RequestWithBody> {
+  constructor(
+    @Inject(RequestWithBodyFromFastifyRequestBuilder)
+    requestBuilder: Builder<RequestWithBody, [FastifyRequest]>,
+    @Inject(PatchGameV1GameIdHttpRequestController)
+    requestController: Handler<
+      [RequestWithBody],
+      Response | ResponseWithBody<unknown>
+    >,
+    @Inject(FastifyReplyFromResponseBuilder)
+    resultBuilder: Builder<
+      FastifyReply,
+      [Response | ResponseWithBody<unknown>, FastifyReply]
+    >,
+  ) {
+    super(requestBuilder, requestController, resultBuilder);
+  }
+
+  @Patch(
+    `:${PatchGameGameIdV1RequestParamHandler.patchGameV1GameIdRequestParam}`,
+  )
+  public override async handle(
+    @Req() request: FastifyRequest,
+    @Res() reply: FastifyReply,
+  ): Promise<void> {
+    await super.handle(request, reply);
+  }
+}
