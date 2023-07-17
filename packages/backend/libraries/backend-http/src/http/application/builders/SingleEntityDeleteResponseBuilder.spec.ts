24df6b6fc2b6d6b68f1ce183542aa525135dbba9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-17 19:53:39 +0200

    refactor(backend-http): add SingleEntityDeleteResponseBuilder

---
+++ packages/backend/libraries/backend-http/src/http/application/builders/SingleEntityDeleteResponseBuilder.spec.ts
@@ -0,0 +1,68 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import httpStatusCodes from 'http-status-codes';
+
+import { Response } from '../models/Response';
+import { ResponseWithBody } from '../models/ResponseWithBody';
+import { SingleEntityDeleteResponseBuilder } from './SingleEntityDeleteResponseBuilder';
+
+describe(SingleEntityDeleteResponseBuilder.name, () => {
+  let singleEntityDeleteResponseBuilder: SingleEntityDeleteResponseBuilder<unknown>;
+
+  beforeAll(() => {
+    singleEntityDeleteResponseBuilder = new SingleEntityDeleteResponseBuilder();
+  });
+
+  describe('.build', () => {
+    describe('having no modelApi', () => {
+      let modelFixture: undefined;
+
+      beforeAll(() => {
+        modelFixture = undefined;
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = singleEntityDeleteResponseBuilder.build(modelFixture);
+        });
+
+        it('should return a Response', () => {
+          const expected: Response = {
+            headers: { 'Content-Type': 'application/json' },
+            statusCode: httpStatusCodes.OK,
+          };
+
+          expect(result).toStrictEqual(expected);
+        });
+      });
+    });
+
+    describe('having a model', () => {
+      let modelFixture: unknown;
+
+      beforeAll(() => {
+        modelFixture = Symbol();
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = singleEntityDeleteResponseBuilder.build(modelFixture);
+        });
+
+        it('should return a Response', () => {
+          const expected: ResponseWithBody<unknown> = {
+            body: modelFixture,
+            headers: { 'Content-Type': 'application/json' },
+            statusCode: httpStatusCodes.OK,
+          };
+
+          expect(result).toStrictEqual(expected);
+        });
+      });
+    });
+  });
+});
