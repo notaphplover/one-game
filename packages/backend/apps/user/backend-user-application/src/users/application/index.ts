70055eb78e447e1a35cfe112c4275ed0ae17d9c6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-20 18:00:06 +0200

    refactor(backend-user-application): update UserHttpApiModule with user code controllers

--- packages/backend/apps/user/backend-user-application/src/users/application/index.ts
+++ packages/backend/apps/user/backend-user-application/src/users/application/index.ts
@@ -1,8 +1,11 @@
+import { DeleteUserV1EmailCodeRequestController } from './controllers/DeleteUserV1EmailCodeRequestController';
 import { DeleteUserV1MeHttpRequestController } from './controllers/DeleteUserV1MeHttpRequestController';
 import { GetUserV1MeHttpRequestController } from './controllers/GetUserV1MeHttpRequestController';
 import { GetUserV1UserIdHttpRequestController } from './controllers/GetUserV1UserIdHttpRequestController';
 import { PatchUserV1MeHttpRequestController } from './controllers/PatchUserV1MeHttpRequestController';
+import { PostUserV1EmailCodeRequestController } from './controllers/PostUserV1EmailCodeRequestController';
 import { PostUserV1HttpRequestController } from './controllers/PostUserV1HttpRequestController';
+import { BaseUserV1EmailCodeRequestParamHandler } from './handlers/BaseUserV1EmailCodeRequestParamHandler';
 import { GetUserV1UserIdRequestParamHandler } from './handlers/GetUserV1UserIdRequestParamHandler';
 import {
   UserCodePersistenceOutputPort,
@@ -14,11 +17,14 @@ import {
 } from './ports/output/UserPersistenceOutputPort';
 
 export {
+  BaseUserV1EmailCodeRequestParamHandler,
+  DeleteUserV1EmailCodeRequestController,
   DeleteUserV1MeHttpRequestController,
   GetUserV1MeHttpRequestController,
   GetUserV1UserIdHttpRequestController,
   GetUserV1UserIdRequestParamHandler,
   PatchUserV1MeHttpRequestController,
+  PostUserV1EmailCodeRequestController,
   PostUserV1HttpRequestController,
   userCodePersistenceOutputPortSymbol,
   userPersistenceOutputPortSymbol,
