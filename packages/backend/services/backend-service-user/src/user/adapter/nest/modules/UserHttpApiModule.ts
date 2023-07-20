28a8f67ec68fda6898306435a124e1ffd3a5f7b0
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-20 18:01:19 +0200

    refactor(backend-service-user): expose user code endpoints

--- packages/backend/services/backend-service-user/src/user/adapter/nest/modules/UserHttpApiModule.ts
+++ packages/backend/services/backend-service-user/src/user/adapter/nest/modules/UserHttpApiModule.ts
@@ -4,19 +4,23 @@ import { Module } from '@nestjs/common';
 
 import { buildDbModuleOptions } from '../../../../foundation/db/adapter/nest/calculations/buildDbModuleOptions';
 import { HttpModule } from '../../../../foundation/http/adapter/nest/modules/HttpModule';
+import { DeleteUserV1EmailCodeRequestNestController } from '../controllers/DeleteUserV1EmailCodeRequestNestController';
 import { DeleteUserV1MeHttpRequestNestController } from '../controllers/DeleteUserV1MeHttpRequestNestController';
 import { GetUserV1MeHttpRequestNestController } from '../controllers/GetUserV1MeHttpRequestNestController';
 import { GetUserV1UserIdHttpRequestNestController } from '../controllers/GetUserV1UserIdHttpRequestNestController';
 import { PatchUserV1MeHttpRequestNestController } from '../controllers/PatchUserV1MeHttpRequestNestController';
+import { PostUserV1EmailCodeRequestNestController } from '../controllers/PostUserV1EmailCodeRequestNestController';
 import { PostUserV1HttpRequestNestController } from '../controllers/PostUserV1HttpRequestNestController';
 
 @Module({
   controllers: [
     // mind the order
+    DeleteUserV1EmailCodeRequestNestController,
     DeleteUserV1MeHttpRequestNestController,
     GetUserV1MeHttpRequestNestController,
     GetUserV1UserIdHttpRequestNestController,
     PatchUserV1MeHttpRequestNestController,
+    PostUserV1EmailCodeRequestNestController,
     PostUserV1HttpRequestNestController,
   ],
   imports: [
