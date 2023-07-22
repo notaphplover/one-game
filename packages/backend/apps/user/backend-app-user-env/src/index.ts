9e13f83309924fedbcbf22738b857b3099eaffd7
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-22 13:04:22 +0200

    refactor(backend-app-user-env): expose MailConfig

--- packages/backend/apps/user/backend-app-user-env/src/index.ts
+++ packages/backend/apps/user/backend-app-user-env/src/index.ts
@@ -1,7 +1,8 @@
 import { EnvModule } from './adapter/nest/modules/EnvModule';
 import { Environment } from './application/models/Environment';
+import { MailConfig } from './application/models/MailConfig';
 import { EnvironmentService } from './application/services/EnvironmentService';
 
-export type { Environment };
+export type { Environment, MailConfig };
 
 export { EnvModule, EnvironmentService };
