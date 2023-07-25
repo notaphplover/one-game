8a80c04c454f635d823ef2b3bd7163f88b9639b2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-25 19:52:02 +0200

    refactor(backend-game-domain): update GameFindQuery with currentPlayingSlotIndex

--- packages/backend/apps/game/backend-game-domain/src/games/domain/query/GameFindQuery.ts
+++ packages/backend/apps/game/backend-game-domain/src/games/domain/query/GameFindQuery.ts
@@ -1,10 +1,12 @@
 import { GameStatus } from '../models/GameStatus';
 import { GameSlotFindQuery } from './GameSlotFindQuery';
+import { GameStateFindQuery } from './GameStateFindQuery';
 
 export interface GameFindQuery {
-  gameSlotFindQuery?: GameSlotFindQuery;
-  id?: string;
-  limit?: number;
-  offset?: number;
-  status?: GameStatus;
+  readonly gameSlotFindQuery?: GameSlotFindQuery;
+  readonly id?: string;
+  readonly limit?: number;
+  readonly offset?: number;
+  readonly state?: GameStateFindQuery;
+  readonly status?: GameStatus;
 }
