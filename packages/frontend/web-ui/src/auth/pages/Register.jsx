cd1179c9792e50a2f8e388b60c748e48a6f2c893
Mario Fernández <mario.fdez.p@gmail.com>
2023-07-27 13:09:14 +0200

    chore(web-ui): create functionality Register page

--- packages/frontend/web-ui/src/auth/pages/Register.jsx
+++ packages/frontend/web-ui/src/auth/pages/Register.jsx
@@ -1,86 +1,182 @@
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { useForm } from '../../cornie/hooks/useForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { Alert, Button, Grid, IconButton, InputAdornment, Link, TextField, Typography } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useForm, useShowPassword } from '../../hooks';
 import { AuthLayout } from '../layout/AuthLayout';
+import { fetchCreateUser } from '../../store/features/auth';
 
 export const Register = () => {
 
-    const {formState, displayName, email, password, onInputChange, onResetForm} = useForm({
-        username: '',
+    const {formState, onInputChange, onResetForm, formValidation, isFormValid} = useForm({
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword: '',
+        component: 'Register'
     });
+    const [ formSubmitted, setFormSubmitted ] = useState(false);
+    const { showPassword, handleClickShowPassword, handleMouseDownPassword } = useShowPassword(false);
+    
+    const dispatch = useDispatch();
+    const { id, errorMessage } = useSelector((state) => state.auth);
+
+    const onSubmit = (event) => {
+
+        event.preventDefault();
+        console.log(`${id} - ${errorMessage}`);
+        console.log(isFormValid);
+        setFormSubmitted(true);
+        if (isFormValid) {
+          dispatch(fetchCreateUser(formState));
+
+          if (errorMessage || id) {
+            onResetForm();
+          }
+            
+          setFormSubmitted(false);
+        } 
+    }
 
     return (
 
-        <AuthLayout title="Create a Cornie's account">
+      <AuthLayout title="Create an account">
 
         <form>
           
-          <Grid container>
-          <Grid item xs={12} sx={{mt: 2}}>
-              <TextField 
+          <Grid container sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '&:hover fieldset': {
+                      borderColor: 'Orchid',
+                    },
+                  },
+                }}>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                autoFocus
+                disabled={id !== null ? true : false}
                 label="Alias" 
                 type="text" 
                 placeholder="alias" 
                 fullWidth 
-                name="alias"
-                value={displayName}
+                name="name"
+                value={formState.name}
                 onChange={onInputChange}
-                //error={emailValid !== null}
-                //helperText={emailValid}
+                error={formValidation.nameValid !== null && formSubmitted}
+                helperText={(formValidation.nameValid && formSubmitted) && formValidation.nameValid}
               />
             </Grid>
             <Grid item xs={12} sx={{mt: 2}}>
               <TextField 
-                label="e-mail" 
+                disabled={id !== null ? true : false}
+                label="Email" 
                 type="email" 
                 placeholder="mail@google.com" 
                 fullWidth 
                 name="email"
-                value={email}
+                value={formState.email}
                 onChange={onInputChange}
-                //error={emailValid !== null}
-                //helperText={emailValid}
+                error={formValidation.emailValid !== null && formSubmitted}
+                helperText={(formValidation.emailValid && formSubmitted) && formValidation.emailValid}
               />
             </Grid>
             <Grid item xs={12} sx={{mt: 2}}>
               <TextField 
+                disabled={id !== null ? true : false}
                 label="Password" 
-                type="password" 
-                placeholder="*******" 
+                type={showPassword ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          color="primary"
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="******" 
                 fullWidth 
                 name="password"
-                value={password}
+                value={formState.password}
                 onChange={onInputChange}
-                //error={passwordValid !== null}
-                //helperText={passwordValid}
+                error={formValidation.passwordValid !== null && formSubmitted}
+                helperText={(formValidation.passwordValid && formSubmitted) && formValidation.passwordValid}
               />
             </Grid>
             <Grid item xs={12} sx={{mt: 2}}>
                 <TextField 
+                  disabled={id !== null ? true : false}
                   label="Confirm Password" 
-                  type="password" 
-                  placeholder="*******" 
+                  type={showPassword ? 'text' : 'password'}
+                  InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            color="primary"
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                  placeholder="******" 
                   fullWidth 
                   name="confirmPassword"
-                  value={password}
+                  value={formState.confirmPassword}
                   onChange={onInputChange}
-                  //error={passwordValid !== null}
-                  //helperText={passwordValid}
+                  error={formValidation.confirmPasswordValid !== null && formSubmitted}
+                  helperText={(formValidation.confirmPasswordValid && formSubmitted) && formValidation.confirmPasswordValid}
                 />
               </Grid>
             </Grid>
 
-          <Grid container spacing={2} sx={{mt: 2, mb: 2}}>
+            <Grid container 
+                  display={errorMessage !== null ? '' : 'none'}>
+              <Grid 
+                item 
+                xs={12}
+                sx={{mt:3}}
+              >
+                <Alert severity='error'>{errorMessage}</Alert>
+              </Grid>
+            </Grid>
+
+            <Grid container 
+                  display={id !== null ? '' : 'none'}>
+              <Grid 
+                item 
+                xs={12}
+                sx={{mt:3}}
+              >
+                <Alert severity='success'>{`User created! We sent an email, please, complete the steps.`}</Alert>
+              </Grid>
+            </Grid>
+
+          <Grid container spacing={2} 
+                sx={{mt: 2, mb: 2, 
+                     '& .Mui-disabled': {
+                        backgroundColor: 'Pink',
+                  },
+          }}>
             
-            <Grid item xs={6} sm={12}>
+            <Grid item xs={12} sm={12}>
               <Button 
-                //disabled={isAuthenticating}
+                disabled={id !== null ? true : false}
                 type="submit" 
                 variant='contained' 
                 fullWidth
-                //onClick={onSubmit}
+                onClick={onSubmit}
               >
                 <Typography textAlign='center'>Create</Typography> 
               </Button>
@@ -93,14 +189,14 @@ export const Register = () => {
               sx={{mt: 4}}
             >
               <Typography sx={{mt: 2, mr:1}}> Do you have a Cornie's account? </Typography>
-              <Link sx={{mt: 2, mr:1}} component={RouterLink} color='inherit' to="/auth/login">
+              <Link sx={{mt: 2, mr:1}} component={RouterLink} color='primary' to="/auth/login">
                 Sign in
               </Link>
             </Grid>
 
           </Grid>
         </form>
-    </AuthLayout>
+      </AuthLayout>
 
     )
 }
---
+++ packages/frontend/web-ui/src/hooks/useShowPassword.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useShowPassword = (initial) => {
+
+    const [showPassword, setShowPassword] = useState(initial);
+
+    const handleClickShowPassword = () => {
+      setShowPassword((show) => !show);
+    }
+  
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+    
+  return {
+    showPassword, 
+    handleClickShowPassword,
+    handleMouseDownPassword
+  }
+}
