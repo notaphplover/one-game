815bc3cbc81b8c47a56e09c18fd09be94db8b290
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-07-30 11:47:13 +0200

    feat(backend-common): add Either

--- packages/backend/libraries/backend-common/src/index.ts
+++ packages/backend/libraries/backend-common/src/index.ts
@@ -6,18 +6,28 @@ import { BuilderAsync } from './common/domain/modules/BuilderAsync';
 import { Converter } from './common/domain/modules/Converter';
 import { ConverterAsync } from './common/domain/modules/ConverterAsync';
 import { Spec } from './common/domain/modules/Spec';
+import {
+  BaseEither,
+  Either,
+  Left,
+  Right,
+} from './common/domain/patterns/fp/Either';
 import { AppError } from './error/application/models/AppError';
 import { AppErrorKind } from './error/application/models/AppErrorKind';
 
 export { AppError, AppErrorKind };
 
 export type {
+  BaseEither,
   Builder,
   BuilderAsync,
+  Either,
   Handler,
   Converter,
   ConverterAsync,
   Entity,
+  Left,
+  Right,
   Spec,
   Writable,
 };
