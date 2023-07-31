06d639ed61af524511e9648a64d60b9570356b88
Mario Fernández <mario.fdez.p@gmail.com>
2023-07-31 15:28:26 +0200

    refactor(web-ui): put AuthLayout in spec. layouts

--- packages/frontend/web-ui/src/auth/layout/LoginLayout.jsx
+++ packages/frontend/web-ui/src/auth/layout/LoginLayout.jsx
@@ -1,6 +1,6 @@
 import { Grid, Typography } from '@mui/material';
 
-export const AuthLayout = ({children, title = ''}) => {
+export const LoginLayout = ({children, title = ''}) => {
     return (
 
         <Grid
