18a80a3332527ddf6364359102cfa376b0bae21d
Mario Fernández <mario.fdez.p@gmail.com>
2023-08-12 18:11:36 +0200

    chore(web-ui): test hook useRegisterForm

---
+++ packages/frontend/web-ui/tests/auth/hooks/useRegisterForm.test.js
@@ -0,0 +1,49 @@
+import { renderHook } from '@testing-library/react';
+import { useRegisterForm } from '../../../src/auth/hooks/useRegisterForm';
+
+describe('Testing in hook useRegisterForm', () => {
+  test('should initial values', () => {
+    const initialForm = {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+    const { result } = renderHook(() => useRegisterForm(initialForm));
+    const { formState, isFormValid } = result.current;
+
+    expect(formState.name).toBe('');
+    expect(isFormValid).toBeFalsy();
+  });
+
+  test('should invalid email and  invalid confirm password', () => {
+    const initialForm = {
+      name: 'Mariote',
+      email: 'marioAgmail.com',
+      password: '123123',
+      confirmPassword: '123456',
+    };
+    const { result } = renderHook(() => useRegisterForm(initialForm));
+    const { formValidation } = result.current;
+
+    expect(formValidation.emailValidationError).toEqual(
+      'The email must be a valid email.',
+    );
+    expect(formValidation.confirmPasswordValidationError).toEqual(
+      'The confirm password must be equal than the password.',
+    );
+  });
+
+  test('should form valid', () => {
+    const initialForm = {
+      name: 'Mariote',
+      email: 'mario@gmail.com',
+      password: '123123',
+      confirmPassword: '123123',
+    };
+    const { result } = renderHook(() => useRegisterForm(initialForm));
+    const { isFormValid } = result.current;
+
+    expect(isFormValid).toBeTruthy();
+  });
+});
