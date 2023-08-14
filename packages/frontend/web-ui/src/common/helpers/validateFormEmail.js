eda31ba5a45d8af8d08e3036ea30d7188ba7e1f0
Mario Fernández <mario.fdez.p@gmail.com>
2023-08-14 16:56:11 +0200

    refactor(web-ui): modify input validations

--- packages/frontend/web-ui/src/common/helpers/validateFormEmail.js
+++ packages/frontend/web-ui/src/common/helpers/validateFormEmail.js
@@ -1,8 +1,12 @@
+/**
+ * @param {Object} formChecked
+ * @param {String} email
+ */
 export const validateFormEmail = (formChecked, email) => {
   const varValidationError = 'ValidationError';
 
-  return email.match(/^\S+@\S+$/gi) == null
-    ? (formChecked[`email${varValidationError}`] =
-        'The email must be a valid email.')
-    : '';
+  if (email.match(/^\S+@\S+$/gi) == null) {
+    formChecked[`email${varValidationError}`] =
+      'The email must be a valid email.';
+  }
 };
