2fc0002085f38db042a2cf62ddf60ee853470edc
Mario Fernández <mario.fdez.p@gmail.com>
2023-08-29 19:39:09 +0200

    chore(web-ui): config babel-preset-vite test hook

--- packages/frontend/web-ui/jest.config.cjs
+++ packages/frontend/web-ui/jest.config.cjs
@@ -1,4 +1,8 @@
+/** @type {!import("@jest/types").Config.InitialOptions} */
 module.exports = {
+  rootDir: '.',
+  roots: ['<rootDir>'],
   testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['<rootDir>/src/**/*.spec.js'],
   transform: { '\\.[jt]sx?$': 'babel-jest' },
 };
