0d4e0b774be222f99fa7d930baa739b82a8adbc2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-08-29 21:05:40 +0200

    refactor(backend-service-game): add HttpModule

--- packages/backend/services/backend-service-game/src/foundation/http/adapter/nest/modules/HttpModule.ts
+++ packages/backend/services/backend-service-game/src/foundation/http/adapter/nest/modules/HttpModule.ts
@@ -1,20 +1,28 @@
 import {
   FastifyReplyFromResponseBuilder,
+  FastifyReplySseConsumerFromFastifyReplyBuilder,
+  FastifySseReplyFromResponseBuilder,
   RequestFromFastifyRequestBuilder,
   RequestWithBodyFromFastifyRequestBuilder,
+  StringifiedSseFromMessageEventBuilder,
 } from '@cornie-js/backend-http';
 import { Module } from '@nestjs/common';
 
 @Module({
   exports: [
     FastifyReplyFromResponseBuilder,
+    FastifyReplySseConsumerFromFastifyReplyBuilder,
+    FastifySseReplyFromResponseBuilder,
     RequestFromFastifyRequestBuilder,
     RequestWithBodyFromFastifyRequestBuilder,
   ],
   providers: [
     FastifyReplyFromResponseBuilder,
+    FastifyReplySseConsumerFromFastifyReplyBuilder,
+    FastifySseReplyFromResponseBuilder,
     RequestFromFastifyRequestBuilder,
     RequestWithBodyFromFastifyRequestBuilder,
+    StringifiedSseFromMessageEventBuilder,
   ],
 })
 export class HttpModule {}
