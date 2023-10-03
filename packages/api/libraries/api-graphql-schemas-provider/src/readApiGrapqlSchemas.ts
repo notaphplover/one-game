eb13d758a7505bb7e3cc5c15550051f629793afd
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-10-03 21:42:39 +0200

    chore: add graphql-schemas-provider package

---
+++ packages/api/libraries/api-graphql-schemas-provider/src/readApiGrapqlSchemas.ts
@@ -0,0 +1,39 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+
+import { glob } from 'glob';
+
+const FILES_BLOB_SUFFIX: string = '**/*.graphql';
+
+const API_PACKAGE: string = '@cornie-js/api-graphql-schemas';
+const API_PACKAGE_SCHEMAS_FOLDER: string = 'schemas';
+
+export async function readApiGraphqlSchemas(): Promise<string[]> {
+  const schemasPackagePath: string = path.dirname(
+    require.resolve(path.join(API_PACKAGE, 'package.json')),
+  );
+
+  const graphqlSchemasBlob: string = path.join(
+    schemasPackagePath,
+    API_PACKAGE_SCHEMAS_FOLDER,
+    FILES_BLOB_SUFFIX,
+  );
+
+  const graphqlSchemasPaths: string[] = await glob(graphqlSchemasBlob);
+
+  const graphqlSchemas: string[] = await Promise.all(
+    graphqlSchemasPaths.map(async (jsonSchemaPath: string) =>
+      parseGraphqlFile(jsonSchemaPath),
+    ),
+  );
+
+  return graphqlSchemas;
+}
+
+async function parseGraphqlFile(path: string): Promise<string> {
+  const fileContentBuffer: Buffer = await fs.readFile(path);
+
+  const fileStringifiedContent: string = fileContentBuffer.toString();
+
+  return fileStringifiedContent;
+}
