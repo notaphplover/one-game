f82cb32d9925faa7ff7b2b1dea98774a4fb724b1
Mario Fernández <mario.fdez.p@gmail.com>
2023-10-09 17:14:57 +0200

    refactor(web-ui): hook useLoginForm changed

--- packages/frontend/web-ui/src/auth/hooks/useLoginForm.js
+++ packages/frontend/web-ui/src/auth/hooks/useLoginForm.js
@@ -1,9 +1,19 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { validateFormEmail, validateFormPassword } from '../../common/helpers';
+import { httpClient } from '../../common/http/services/HttpService';
+import { buildSerializableResponse } from '../../common/http/helpers/buildSerializableResponse';
+
+export const STATUS_LOG_INITIAL = 0;
+export const STATUS_LOG_PENDING_VAL = 1;
+export const STATUS_LOG_VALIDATION_KO = 2;
+export const STATUS_LOG_PENDING_BACKEND = 3;
+export const STATUS_LOG_BACKEND_KO = 4;
+export const STATUS_LOG_BACKEND_OK = 5;
 
 export const useLoginForm = (initialForm = {}) => {
-  const [formState, setFormState] = useState(initialForm);
+  const [formFields, setFormFields] = useState(initialFormFields);
+  const [formStatus, setFormStatus] = useState(STATUS_LOG_INITIAL);
+  const [backendError, setBackendError] = useState(null);
   const [formValidation, setFormValidation] = useState({});
 
   const isFormValid = useMemo(() => {
@@ -14,35 +24,62 @@ export const useLoginForm = (initialForm = {}) => {
   });
 
   useEffect(() => {
-    createValidators();
-  }, [formState]);
+    switch (formStatus) {
+      case STATUS_LOG_PENDING_VAL:
+        validateFormFields();
+        break;
+      case STATUS_LOG_PENDING_BACKEND:
+        //createUser(formFields);  <--- GET A TOKEN
+        break;
+      default:
+    }
+  }, [formFields]);
 
-  const onInputChange = ({ target }) => {
+  const setFormField = ({ target }) => {
+    if (
+      formStatus !== STATUS_LOG_INITIAL &&
+      formStatus !== STATUS_LOG_VALIDATION_KO
+    ) {
+      throw new Error('Unexpected form state at setFormField');
+    }
+
     const { name, value } = target;
-    setFormState({
-      ...formState,
+    setFormFields({
+      ...formFields,
       [name]: value,
     });
   };
 
-  const onResetForm = () => {
-    setFormState(initialForm);
+  const notifyFormFieldsFilled = () => {
+    setFormStatus(STATUS_LOG_PENDING_VAL);
+    setBackendError(null);
   };
 
-  const createValidators = () => {
-    const formCheckedValues = {};
+  const validateFormFields = () => {
+    if (formStatus !== STATUS_LOG_PENDING_VAL) {
+      throw new Error('Unexpected form state at validateFormFields');
+    }
 
-    validateFormEmail(formCheckedValues, formState.email);
-    validateFormPassword(formCheckedValues, formState.password);
+    const formValidationValue = {};
 
-    setFormValidation(formCheckedValues);
+    validateFormEmail(formValidationValue, formFields.email);
+    validateFormPassword(formValidationValue, formFields.password);
+
+    setFormValidation(formValidationValue);
+
+    if (Object.values(formValidationValue).length === 0) {
+      setFormStatus(STATUS_LOG_PENDING_BACKEND);
+    } else {
+      setFormStatus(STATUS_LOG_VALIDATION_KO);
+    }
   };
 
   return {
-    formState,
-    onInputChange,
-    onResetForm,
+    backendError,
+    formFields,
+    formStatus,
     formValidation,
-    isFormValid,
+    notifyFormFieldsFilled,
+    setFormField,
   };
 };
