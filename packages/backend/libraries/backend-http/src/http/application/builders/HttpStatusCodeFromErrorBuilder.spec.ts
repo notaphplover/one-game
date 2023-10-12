d31579192acc75848ff3bf615f226b78972a261c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-10-12 11:45:07 +0200

    refactor(backend-http): add HttpStatusCodeFromErrorBuilder

---
+++ packages/backend/libraries/backend-http/src/http/application/builders/HttpStatusCodeFromErrorBuilder.spec.ts
@@ -0,0 +1,41 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { AppError, AppErrorKind } from '@cornie-js/backend-common';
+import { HttpStatus } from '@nestjs/common';
+
+import { HttpStatusCodeFromErrorBuilder } from './HttpStatusCodeFromErrorBuilder';
+
+describe(HttpStatusCodeFromErrorBuilder.name, () => {
+  let httpStatusCodeFromErrorBuilder: HttpStatusCodeFromErrorBuilder;
+
+  beforeAll(() => {
+    httpStatusCodeFromErrorBuilder = new HttpStatusCodeFromErrorBuilder();
+  });
+
+  describe.each<[AppErrorKind, number]>([
+    [AppErrorKind.contractViolation, HttpStatus.BAD_REQUEST],
+    [AppErrorKind.entityNotFound, HttpStatus.NOT_FOUND],
+    [AppErrorKind.entityConflict, HttpStatus.CONFLICT],
+    [AppErrorKind.invalidCredentials, HttpStatus.FORBIDDEN],
+    [AppErrorKind.missingCredentials, HttpStatus.UNAUTHORIZED],
+    [AppErrorKind.unknown, HttpStatus.INTERNAL_SERVER_ERROR],
+    [AppErrorKind.unprocessableOperation, HttpStatus.UNPROCESSABLE_ENTITY],
+  ])(
+    'having an error with "%s" kind',
+    (appErrorKind: AppErrorKind, expectedResult: number) => {
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = httpStatusCodeFromErrorBuilder.build(
+            new AppError(appErrorKind),
+          );
+        });
+
+        it(`should return "${expectedResult}" HTTP status code`, () => {
+          expect(result).toBe(expectedResult);
+        });
+      });
+    },
+  );
+});
