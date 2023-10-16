0856c63ea43f1b97df0c5b660dd1253b1baa860a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-10-17 01:23:47 +0200

    feat(backend-service-gateway): update apollo server resolver with an error handle strategy

---
+++ packages/backend/services/backend-service-gateway/src/app/adapter/apollo/calculations/buildApolloApplicationResolver.ts
@@ -0,0 +1,65 @@
+import { models as graphqlModels } from '@cornie-js/api-graphql-models';
+import { Builder } from '@cornie-js/backend-common';
+import { ApplicationResolver } from '@cornie-js/backend-gateway-application';
+import { Request } from '@cornie-js/backend-http';
+import { GraphQLError, GraphQLResolveInfo } from 'graphql';
+
+export function buildApolloApplicationResolver(
+  applicationResolver: ApplicationResolver,
+  graphQlErrorFromErrorBuilder: Builder<GraphQLError, [unknown]>,
+): ApplicationResolver {
+  return {
+    RootMutation: {
+      createAuthByCode: buildApolloResolver(
+        applicationResolver.RootMutation,
+        graphQlErrorFromErrorBuilder,
+        applicationResolver.RootMutation.createAuthByCode,
+      ),
+      createAuthByCredentials: buildApolloResolver(
+        applicationResolver.RootMutation,
+        graphQlErrorFromErrorBuilder,
+        applicationResolver.RootMutation.createAuthByCredentials,
+      ),
+      createUser: buildApolloResolver(
+        applicationResolver.RootMutation,
+        graphQlErrorFromErrorBuilder,
+        applicationResolver.RootMutation.createUser,
+      ),
+    },
+  };
+}
+
+function getResolverFunction<TParent, TResult, TArgs>(
+  self: unknown,
+  resolver: graphqlModels.Resolver<TResult, TParent, Request, TArgs>,
+): graphqlModels.ResolverFn<TResult, TParent, Request, TArgs> {
+  if (typeof resolver === 'function') {
+    return resolver.bind(self);
+  } else {
+    return resolver.resolve.bind(self);
+  }
+}
+
+function buildApolloResolver<TParent, TResult, TArgs>(
+  self: unknown,
+  graphQlErrorFromErrorBuilder: Builder<GraphQLError, [unknown]>,
+  resolver: graphqlModels.Resolver<TResult, TParent, Request, TArgs>,
+): graphqlModels.ResolverFn<TResult, TParent, Request, TArgs> {
+  return async (
+    parent: TParent,
+    args: TArgs,
+    context: Request,
+    info: GraphQLResolveInfo,
+  ): Promise<TResult> => {
+    try {
+      return await getResolverFunction(self, resolver)(
+        parent,
+        args,
+        context,
+        info,
+      );
+    } catch (error: unknown) {
+      throw graphQlErrorFromErrorBuilder.build(error);
+    }
+  };
+}
--- packages/backend/services/backend-service-gateway/src/app/adapter/nest/calculations/buildApolloServer.ts
+++ packages/backend/services/backend-service-gateway/src/app/adapter/nest/calculations/buildApolloServer.ts
@@ -1,10 +1,15 @@
 import { ApolloServer } from '@apollo/server';
 import { fastifyApolloDrainPlugin } from '@as-integrations/fastify';
 import { readApiGraphqlSchemas } from '@cornie-js/api-graphql-schemas-provider';
+import { Builder } from '@cornie-js/backend-common';
 import { ApplicationResolver } from '@cornie-js/backend-gateway-application';
+import { GraphQlErrorFromErrorBuilder } from '@cornie-js/backend-graphql';
 import { Request } from '@cornie-js/backend-http';
 import { INestApplicationContext } from '@nestjs/common';
 import { FastifyInstance } from 'fastify';
+import { GraphQLError } from 'graphql';
+
+import { buildApolloApplicationResolver } from '../../apollo/calculations/buildApolloApplicationResolver';
 
 export async function buildApolloServer(
   fastifyServer: FastifyInstance,
@@ -12,14 +17,20 @@ export async function buildApolloServer(
 ): Promise<ApolloServer<Request>> {
   const graphqlSchemas: string[] = await readApiGraphqlSchemas();
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
-  const applicationResolver: any =
+  const applicationResolver: ApplicationResolver =
     nestApplicationContext.get(ApplicationResolver);
+
+  const graphQlErrorFromErrorBuilder: Builder<GraphQLError, [unknown]> =
+    nestApplicationContext.get(GraphQlErrorFromErrorBuilder);
 
   const apolloServer: ApolloServer<Request> = new ApolloServer({
     plugins: [fastifyApolloDrainPlugin(fastifyServer)],
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    resolvers: applicationResolver,
+    resolvers: buildApolloApplicationResolver(
+      applicationResolver,
+      graphQlErrorFromErrorBuilder,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ) as any,
     typeDefs: graphqlSchemas,
   });
 
