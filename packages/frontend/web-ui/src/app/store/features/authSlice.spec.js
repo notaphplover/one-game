4a3234d32efa32f9322de41b1313700a6a300c28
Mario Fernández <mario.fdez.p@gmail.com>
2023-10-19 17:21:23 +0200

    test(web-ui): updated test authSlice with thunk createAuthByCredentials

--- packages/frontend/web-ui/src/app/store/features/authSlice.spec.js
+++ packages/frontend/web-ui/src/app/store/features/authSlice.spec.js
@@ -1,6 +1,7 @@
 import { beforeAll, describe, expect, it } from '@jest/globals';
 import authSlice from './authSlice';
 import { createAuthByToken } from '../thunk/createAuthByToken';
+import { createAuthByCredentials } from '../thunk/createAuthByCredentials';
 
 describe('authSlice', () => {
   describe('having no state and no action', () => {
@@ -171,6 +172,143 @@ describe('authSlice', () => {
         errorMessage: null,
       };
       actionFixture = createAuthByToken.fulfilled(payloadFixture);
+    });
+
+    describe('when authSlice.reducer() is called', () => {
+      let result;
+
+      beforeAll(() => {
+        result = authSlice.reducer(stateFixture, actionFixture);
+      });
+
+      it('should return a state', () => {
+        expect(result).toStrictEqual({
+          status: 'not-authenticated',
+          token: null,
+          errorMessage: 'Ups... something strange happened. Try again?',
+        });
+      });
+    });
+  });
+
+  describe('having a non authenticated state and a createAuthByCredentials pending action', () => {
+    let stateFixture;
+    let actionFixture;
+
+    beforeAll(() => {
+      stateFixture = {
+        status: 'not-authenticated',
+        token: null,
+        errorMessage: null,
+      };
+      actionFixture = createAuthByCredentials.pending();
+    });
+
+    describe('when authSlice.reducer() is called', () => {
+      let result;
+
+      beforeAll(() => {
+        result = authSlice.reducer(stateFixture, actionFixture);
+      });
+
+      it('should return a state', () => {
+        expect(result).toStrictEqual({
+          status: 'checking',
+          token: null,
+          errorMessage: null,
+        });
+      });
+    });
+  });
+
+  describe('having a checking state and a createAuthByCredentials fulfilled action with an HTTP OK status code', () => {
+    let payloadFixture;
+    let stateFixture;
+    let actionFixture;
+
+    beforeAll(() => {
+      payloadFixture = {
+        body: {
+          jwt: 'jwt-fixture',
+        },
+        statusCode: 200,
+      };
+      stateFixture = {
+        status: 'checking',
+        token: null,
+        errorMessage: null,
+      };
+      actionFixture = createAuthByCredentials.fulfilled(payloadFixture);
+    });
+
+    describe('when authSlice.reducer() is called', () => {
+      let result;
+
+      beforeAll(() => {
+        result = authSlice.reducer(stateFixture, actionFixture);
+      });
+
+      it('should return a state', () => {
+        expect(result).toStrictEqual({
+          status: 'authenticated',
+          token: payloadFixture.body.jwt,
+          errorMessage: null,
+        });
+      });
+    });
+  });
+
+  describe('having a checking state and a createAuthByCredentials fulfilled action with an HTTP UNPROCESSABLE ENTITY status code', () => {
+    let payloadFixture;
+    let stateFixture;
+    let actionFixture;
+
+    beforeAll(() => {
+      payloadFixture = {
+        body: {},
+        statusCode: 422,
+      };
+      stateFixture = {
+        status: 'checking',
+        token: null,
+        errorMessage: null,
+      };
+      actionFixture = createAuthByCredentials.fulfilled(payloadFixture);
+    });
+
+    describe('when authSlice.reducer() is called', () => {
+      let result;
+
+      beforeAll(() => {
+        result = authSlice.reducer(stateFixture, actionFixture);
+      });
+
+      it('should return a state', () => {
+        expect(result).toStrictEqual({
+          status: 'not-authenticated',
+          token: null,
+          errorMessage: 'Unprocessable operation. Try again.',
+        });
+      });
+    });
+  });
+
+  describe('having a checking state and a createAuthByCredentials fulfilled action with another HTTP status code', () => {
+    let payloadFixture;
+    let stateFixture;
+    let actionFixture;
+
+    beforeAll(() => {
+      payloadFixture = {
+        body: {},
+        statusCode: 500,
+      };
+      stateFixture = {
+        status: 'checking',
+        token: null,
+        errorMessage: null,
+      };
+      actionFixture = createAuthByCredentials.fulfilled(payloadFixture);
     });
 
     describe('when authSlice.reducer() is called', () => {
