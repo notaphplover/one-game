f19529b83bff78e08fe30f0ebd11db845de90ddd
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-11-10 21:50:56 +0100

    refactor(backend-gateway-application): update ApplicationResolver to use GameResolver

--- packages/backend/apps/gateway/backend-gateway-application/src/app/application/resolvers/ApplicationResolver.ts
+++ packages/backend/apps/gateway/backend-gateway-application/src/app/application/resolvers/ApplicationResolver.ts
@@ -2,22 +2,28 @@ import { models as graphqlModels } from '@cornie-js/api-graphql-models';
 import { Request } from '@cornie-js/backend-http';
 import { Inject, Injectable } from '@nestjs/common';
 
+import { GameResolver } from '../../../games/application/resolvers/GameResolver';
 import { RootMutationResolver } from './RootMutationResolver';
 import { RootQueryResolver } from './RootQueryResolver';
 
 @Injectable()
 export class ApplicationResolver implements Partial<graphqlModels.Resolvers> {
   // eslint-disable-next-line @typescript-eslint/naming-convention
+  public readonly Game: graphqlModels.GameResolvers<Request>;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   public readonly RootMutation: graphqlModels.RootMutationResolvers<Request>;
   // eslint-disable-next-line @typescript-eslint/naming-convention
   public readonly RootQuery: graphqlModels.RootQueryResolvers<Request>;
 
   constructor(
+    @Inject(GameResolver)
+    gameResolver: graphqlModels.GameResolvers<Request>,
     @Inject(RootMutationResolver)
     rootMutationResolver: graphqlModels.RootMutationResolvers<Request>,
     @Inject(RootQueryResolver)
     rootQueryResolver: graphqlModels.RootQueryResolvers<Request>,
   ) {
+    this.Game = gameResolver;
     this.RootMutation = rootMutationResolver;
     this.RootQuery = rootQueryResolver;
   }
