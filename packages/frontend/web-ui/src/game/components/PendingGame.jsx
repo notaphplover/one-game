bcf9f929a8ee121845dc2063ab4125fa10f2f853
Mario Fernández <mario.fdez.p@gmail.com>
2023-11-13 17:22:55 +0100

    chore(web-ui): add name to game pending

--- packages/frontend/web-ui/src/game/components/PendingGame.jsx
+++ packages/frontend/web-ui/src/game/components/PendingGame.jsx
@@ -2,6 +2,7 @@ import { Button, Grid, Typography, Link } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const PendingGame = ({ game }) => {
+  console.log(game);
   return (
     <Grid item xs={3}>
       <Grid component="div" className="pending-game">
@@ -11,7 +12,7 @@ export const PendingGame = ({ game }) => {
             className="home-auth-text-game"
             component="h5"
           >
-            {game.id.substring(1, 18)}
+            {game.name === undefined ? '<Unknown>' : game.name}
           </Typography>
         </Grid>
         <Grid item className="pending-button-game">
