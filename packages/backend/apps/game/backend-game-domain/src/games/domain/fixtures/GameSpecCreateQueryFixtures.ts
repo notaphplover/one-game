9cd7e5f0b5b14b6bc93163d85da3d708b222dd7b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2023-11-24 21:08:39 +0100

    refactor(backend-game-domain): update GameSpecCreateQuery with gameId and id

---
+++ packages/backend/apps/game/backend-game-domain/src/games/domain/fixtures/GameSpecCreateQueryFixtures.ts
@@ -0,0 +1,15 @@
+import { GameSpecCreateQuery } from '../query/GameSpecCreateQuery';
+import { GameCardSpecFixtures } from './GameCardSpecFixtures';
+import { GameOptionsFixtures } from './GameOptionsFixtures';
+
+export class GameSpecCreateQueryFixtures {
+  public static get any(): GameSpecCreateQuery {
+    return {
+      cards: [GameCardSpecFixtures.any],
+      gameId: 'e6b54159-a4ef-41fc-994a-20709526bdaa',
+      gameSlotsAmount: 2,
+      id: 'e3b54159-a4ef-41fc-094a-20709526bdaf',
+      options: GameOptionsFixtures.any,
+    };
+  }
+}
