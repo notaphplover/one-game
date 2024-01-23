d364443d0fbc272bf61fb43b3d916193f3d1b387
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-01-23 23:35:52 +0100

    refactor(backend-db): remove legacy typeorm services

--- packages/backend/libraries/backend-db/src/index.ts
+++ packages/backend/libraries/backend-db/src/index.ts
@@ -1,30 +1,16 @@
 import { FindQueryTypeOrmFromQueryBuilder } from './persistence/builders/typeorm/FindQueryTypeOrmFromQueryBuilder';
-import { QueryToFindQueryTypeOrmConverter } from './persistence/converters/typeorm/QueryToFindQueryTypeOrmConverter';
-import { DeleteTypeOrmService } from './persistence/services/typeorm/DeleteTypeOrmService';
 import { DeleteTypeOrmServiceV2 } from './persistence/services/typeorm/DeleteTypeOrmServiceV2';
-import { FindTypeOrmService } from './persistence/services/typeorm/FindTypeOrmService';
 import { FindTypeOrmServiceV2 } from './persistence/services/typeorm/FindTypeOrmServiceV2';
-import { InsertTypeOrmService } from './persistence/services/typeorm/InsertTypeOrmService';
 import { InsertTypeOrmServiceV2 } from './persistence/services/typeorm/InsertTypeOrmServiceV2';
-import { InsertTypeOrmPostgresService } from './persistence/services/typeorm/postgres/InsertTypeOrmPostgresService';
 import { InsertTypeOrmPostgresServiceV2 } from './persistence/services/typeorm/postgres/InsertTypeOrmPostgresServiceV2';
-import { UpdateTypeOrmService } from './persistence/services/typeorm/UpdateTypeOrmService';
 import { UpdateTypeOrmServiceV2 } from './persistence/services/typeorm/UpdateTypeOrmServiceV2';
 
-export type {
-  FindQueryTypeOrmFromQueryBuilder,
-  QueryToFindQueryTypeOrmConverter,
-};
+export type { FindQueryTypeOrmFromQueryBuilder };
 
 export {
-  DeleteTypeOrmService,
   DeleteTypeOrmServiceV2,
-  FindTypeOrmService,
   FindTypeOrmServiceV2,
-  InsertTypeOrmService,
   InsertTypeOrmServiceV2,
-  InsertTypeOrmPostgresService,
   InsertTypeOrmPostgresServiceV2,
-  UpdateTypeOrmService,
   UpdateTypeOrmServiceV2,
 };
