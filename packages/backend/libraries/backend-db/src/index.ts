8b773d9a356e065c8a428d3d7e123f3ae9814cc2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-01-25 19:47:23 +0100

    refactor(backend-db): add DeleteTypeOrmQueryBuilderService

--- packages/backend/libraries/backend-db/src/index.ts
+++ packages/backend/libraries/backend-db/src/index.ts
@@ -1,4 +1,5 @@
 import { FindQueryTypeOrmFromQueryBuilder } from './persistence/builders/typeorm/FindQueryTypeOrmFromQueryBuilder';
+import { DeleteTypeOrmQueryBuilderService } from './persistence/services/typeorm/DeleteTypeOrmQueryBuilderService';
 import { DeleteTypeOrmServiceV2 } from './persistence/services/typeorm/DeleteTypeOrmServiceV2';
 import { FindTypeOrmServiceV2 } from './persistence/services/typeorm/FindTypeOrmServiceV2';
 import { InsertTypeOrmServiceV2 } from './persistence/services/typeorm/InsertTypeOrmServiceV2';
@@ -9,6 +10,7 @@ export type { FindQueryTypeOrmFromQueryBuilder };
 
 export {
   DeleteTypeOrmServiceV2,
+  DeleteTypeOrmQueryBuilderService,
   FindTypeOrmServiceV2,
   InsertTypeOrmServiceV2,
   InsertTypeOrmPostgresServiceV2,
---
+++ packages/backend/libraries/backend-db/src/persistence/services/typeorm/DeleteTypeOrmQueryBuilderService.int.spec.ts
@@ -0,0 +1,314 @@
+import path from 'path';
+
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+
+import { Builder, BuilderAsync } from '@cornie-js/backend-common';
+import {
+  Column,
+  ColumnType,
+  DataSource,
+  DataSourceOptions,
+  Entity,
+  FindManyOptions,
+  ObjectLiteral,
+  PrimaryColumn,
+  QueryBuilder,
+  QueryRunner,
+  Repository,
+  Table,
+  TableColumn,
+  WhereExpressionBuilder,
+} from 'typeorm';
+
+import { DeleteTypeOrmQueryBuilderService } from './DeleteTypeOrmQueryBuilderService';
+
+function getModelTestTable(fooColumnName: string, idColumnName: string): Table {
+  const modelTestTableName: string = 'model_test';
+
+  const modelTestTable: Table = new Table({
+    columns: [
+      {
+        isPrimary: false,
+        length: '128',
+        name: fooColumnName,
+        type: 'varchar',
+      },
+      {
+        isPrimary: true,
+        length: '36',
+        name: idColumnName,
+        type: 'varchar',
+      },
+    ],
+    name: modelTestTableName,
+  });
+
+  return modelTestTable;
+}
+
+async function setupModelTestTable(
+  queryRunner: QueryRunner,
+  modelTestTable: Table,
+): Promise<void> {
+  await tearDownModelTestTable(queryRunner, modelTestTable);
+
+  await queryRunner.createTable(modelTestTable);
+}
+
+async function tearDownModelTestTable(
+  queryRunner: QueryRunner,
+  modelTestTable: Table,
+): Promise<void> {
+  if (await queryRunner.hasTable(modelTestTable)) {
+    await queryRunner.dropTable(modelTestTable);
+  }
+}
+
+function decorateModelTest(
+  modelTestTable: Table,
+  fooColumnName: keyof ModelTest,
+  idColumnName: keyof ModelTest,
+): void {
+  const idColumn: TableColumn = modelTestTable.columns.find(
+    (tableColumn: TableColumn) => tableColumn.name === idColumnName,
+  ) as TableColumn;
+  const fooColumn: TableColumn = modelTestTable.columns.find(
+    (tableColumn: TableColumn) => tableColumn.name === fooColumnName,
+  ) as TableColumn;
+
+  PrimaryColumn({
+    length: idColumn.length,
+    name: idColumn.name,
+    type: idColumn.type as ColumnType,
+  })(ModelTest.prototype, idColumn.name);
+
+  Column({
+    length: fooColumn.length,
+    name: fooColumn.name,
+    type: fooColumn.type as ColumnType,
+  })(ModelTest.prototype, fooColumn.name);
+
+  Entity(modelTestTable.name)(ModelTest);
+}
+
+class ModelTest {
+  public id!: string;
+  public foo!: string;
+}
+
+interface QueryTest {
+  fooValue: string;
+}
+
+describe(DeleteTypeOrmQueryBuilderService.name, () => {
+  let modelTestTable: Table;
+  let datasource: DataSource;
+  let queryRunner: QueryRunner;
+
+  let modelTestRepository: Repository<ModelTest>;
+  let findQueryTypeOrmFromQueryBuilderMock: jest.Mocked<
+    | Builder<
+        QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+        [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+      >
+    | BuilderAsync<
+        QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+        [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+      >
+  >;
+
+  let deleteTypeOrmQueryBuilderService: DeleteTypeOrmQueryBuilderService<
+    ModelTest,
+    QueryTest
+  >;
+
+  beforeAll(async () => {
+    const fooColumnName: keyof ModelTest = 'foo';
+    const idColumnName: keyof ModelTest = 'id';
+
+    modelTestTable = getModelTestTable(fooColumnName, idColumnName);
+
+    decorateModelTest(modelTestTable, fooColumnName, idColumnName);
+
+    const datasourceOptions: DataSourceOptions = {
+      database: path.resolve(
+        'tmp',
+        'typeorm',
+        'DeleteTypeOrmQueryBuilderService.sqlite',
+      ),
+      entities: [ModelTest],
+      logging: false,
+      type: 'sqlite',
+    };
+
+    datasource = new DataSource(datasourceOptions);
+
+    await datasource.initialize();
+
+    queryRunner = datasource.createQueryRunner();
+
+    await setupModelTestTable(queryRunner, modelTestTable);
+
+    modelTestRepository = datasource.getRepository(ModelTest);
+    findQueryTypeOrmFromQueryBuilderMock = {
+      build: jest.fn(),
+    } as Partial<
+      jest.Mocked<
+        | Builder<
+            QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+            [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+          >
+        | BuilderAsync<
+            QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+            [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+          >
+      >
+    > as jest.Mocked<
+      | Builder<
+          QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+          [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+        >
+      | BuilderAsync<
+          QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+          [QueryTest, QueryBuilder<ObjectLiteral> & WhereExpressionBuilder]
+        >
+    >;
+
+    deleteTypeOrmQueryBuilderService = new DeleteTypeOrmQueryBuilderService<
+      ModelTest,
+      QueryTest
+    >(modelTestRepository, findQueryTypeOrmFromQueryBuilderMock);
+  });
+
+  describe('.delete', () => {
+    describe('having a model', () => {
+      let modelTest: ModelTest;
+
+      beforeAll(() => {
+        modelTest = new ModelTest();
+        modelTest.id = '836d2558-0f11-4c63-beb9-e78edba50428';
+        modelTest.foo = 'some foo value';
+      });
+
+      describe('when called, and model is on db and findQueryTypeOrmFromQueryBuilder.build() returns a typeorm find query matching a ModelTest', () => {
+        beforeAll(async () => {
+          await modelTestRepository.save(modelTest);
+
+          const queryTest: QueryTest = {
+            fooValue: 'blah',
+          };
+
+          (
+            findQueryTypeOrmFromQueryBuilderMock as jest.Mocked<
+              Builder<
+                QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+                [
+                  QueryTest,
+                  QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+                ]
+              >
+            >
+          ).build.mockImplementationOnce(
+            (
+              _query: QueryTest,
+              queryBuilder: QueryBuilder<ObjectLiteral> &
+                WhereExpressionBuilder,
+            ): QueryBuilder<ObjectLiteral> & WhereExpressionBuilder =>
+              queryBuilder.andWhere(`id = :id`, { [`id`]: modelTest.id }),
+          );
+
+          await deleteTypeOrmQueryBuilderService.delete(queryTest);
+        });
+
+        afterAll(async () => {
+          await modelTestRepository.delete({
+            id: modelTest.id,
+          });
+
+          jest.clearAllMocks();
+        });
+
+        describe('when called and modelTestRepository.findOne() with a typeorm find query matching the ModelTest', () => {
+          let result: unknown;
+
+          beforeAll(async () => {
+            const queryTypeOrmFixture: FindManyOptions<ModelTest> = {
+              where: {
+                id: modelTest.id,
+              },
+            };
+
+            result = await modelTestRepository.findOne(queryTypeOrmFixture);
+          });
+
+          it('should return null', () => {
+            expect(result).toBeNull();
+          });
+        });
+      });
+
+      describe('when called, and model is on db and findQueryTypeOrmFromQueryBuilderMock.build() returns a typeorm find query not matching a ModelTest', () => {
+        beforeAll(async () => {
+          await modelTestRepository.save(modelTest);
+
+          const queryTest: QueryTest = {
+            fooValue: 'blah',
+          };
+
+          (
+            findQueryTypeOrmFromQueryBuilderMock as jest.Mocked<
+              Builder<
+                QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+                [
+                  QueryTest,
+                  QueryBuilder<ObjectLiteral> & WhereExpressionBuilder,
+                ]
+              >
+            >
+          ).build.mockImplementationOnce(
+            (
+              _query: QueryTest,
+              queryBuilder: QueryBuilder<ObjectLiteral> &
+                WhereExpressionBuilder,
+            ): QueryBuilder<ObjectLiteral> & WhereExpressionBuilder =>
+              queryBuilder.andWhere(`id != :id`, { [`id`]: modelTest.id }),
+          );
+
+          await deleteTypeOrmQueryBuilderService.delete(queryTest);
+        });
+
+        afterAll(async () => {
+          await modelTestRepository.delete({
+            id: modelTest.id,
+          });
+
+          jest.clearAllMocks();
+        });
+
+        describe('when called modelTestRepository.findOne() with a typeorm find query matching the ModelTest', () => {
+          let result: unknown;
+
+          beforeAll(async () => {
+            const queryTypeOrmFixture: FindManyOptions<ModelTest> = {
+              where: {
+                id: modelTest.id,
+              },
+            };
+
+            result = await modelTestRepository.findOne(queryTypeOrmFixture);
+          });
+
+          it('should return a model test', () => {
+            expect(result).toStrictEqual(modelTest);
+          });
+        });
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await tearDownModelTestTable(queryRunner, modelTestTable);
+
+    await datasource.destroy();
+  });
+});
