8cf9cb2a5c012add8378f9c6167cc188b465803a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-01-25 19:59:23 +0100

    refactor(backend-db): add FindTypeOrmQueryBuilderService

--- packages/backend/libraries/backend-db/src/index.ts
+++ packages/backend/libraries/backend-db/src/index.ts
@@ -1,6 +1,7 @@
 import { FindQueryTypeOrmFromQueryBuilder } from './persistence/builders/typeorm/FindQueryTypeOrmFromQueryBuilder';
 import { DeleteTypeOrmQueryBuilderService } from './persistence/services/typeorm/DeleteTypeOrmQueryBuilderService';
 import { DeleteTypeOrmServiceV2 } from './persistence/services/typeorm/DeleteTypeOrmServiceV2';
+import { FindTypeOrmQueryBuilderService } from './persistence/services/typeorm/FindTypeOrmQueryBuilderService';
 import { FindTypeOrmServiceV2 } from './persistence/services/typeorm/FindTypeOrmServiceV2';
 import { UpdateTypeOrmQueryBuilderService } from './persistence/services/typeorm/UpdateTypeOrmQueryBuilderService';
 import { UpdateTypeOrmServiceV2 } from './persistence/services/typeorm/UpdateTypeOrmServiceV2';
@@ -10,6 +11,7 @@ export type { FindQueryTypeOrmFromQueryBuilder };
 export {
   DeleteTypeOrmQueryBuilderService,
   DeleteTypeOrmServiceV2,
+  FindTypeOrmQueryBuilderService,
   FindTypeOrmServiceV2,
   UpdateTypeOrmQueryBuilderService,
   UpdateTypeOrmServiceV2,
