8b1aba3c83170e6e08bbed52a3707a77a79b8d24
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-01-31 02:09:49 +0100

    refactor(backend-user-application): update UserCreatedEvent with optional transactionContext

--- packages/backend/apps/user/backend-user-application/src/users/application/models/UserCreatedEvent.ts
+++ packages/backend/apps/user/backend-user-application/src/users/application/models/UserCreatedEvent.ts
@@ -1,6 +1,8 @@
+import { TransactionContext } from '@cornie-js/backend-db/application';
 import { User, UserCreateQuery } from '@cornie-js/backend-user-domain/users';
 
 export interface UserCreatedEvent {
+  transactionContext?: TransactionContext;
   user: User;
   userCreateQuery: UserCreateQuery;
 }
