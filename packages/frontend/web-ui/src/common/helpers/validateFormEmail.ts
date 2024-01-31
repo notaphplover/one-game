38d506a5445a1c378e252523b5c453e99ccd9611
Mario Fernández <mario.fdez.p@gmail.com>
2024-01-31 15:37:25 +0100

    refactor(web-ui): changed validateFormEmail from JS to TS

--- packages/frontend/web-ui/src/common/helpers/validateFormEmail.ts
+++ packages/frontend/web-ui/src/common/helpers/validateFormEmail.ts
@@ -2,7 +2,10 @@
  * @param {Object} formChecked
  * @param {String} email
  */
-export const validateFormEmail = (formChecked, email) => {
+export const validateFormEmail = (
+  formChecked: { email: string },
+  email: string,
+) => {
   if (email.match(/^\S+@\S+$/gi) == null) {
     formChecked.email = 'The email must be a valid email.';
   }
