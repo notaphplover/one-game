fd9270b23ef689a40e8e97d94f5cd0e9e2cdecba
Mario Fernández <mario.fdez.p@gmail.com>
2024-01-31 15:45:01 +0100

    refactor(web-ui): changed validateFormName validation file from JS to TS

--- packages/frontend/web-ui/src/common/helpers/validateFormName.ts
+++ packages/frontend/web-ui/src/common/helpers/validateFormName.ts
@@ -2,7 +2,10 @@
  * @param {Object} formChecked
  * @param {String} name
  */
-export const validateFormName = (formChecked, name) => {
+export const validateFormName = (
+  formChecked: { name: string },
+  name: string,
+) => {
   if (name.trim() === '') {
     formChecked.name = 'The name is mandatory.';
   }
