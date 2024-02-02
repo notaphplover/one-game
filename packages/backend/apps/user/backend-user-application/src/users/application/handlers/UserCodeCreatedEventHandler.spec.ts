1a0c94d0790249f9ceca49a9f7bf2bbd97c35e61
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-02-02 19:09:32 +0100

    refactor(backend-user-application): update UserCodeCreatedEventHandler
    update module with user activation flow

--- packages/backend/apps/user/backend-user-application/src/users/application/handlers/UserCodeCreatedEventHandler.spec.ts
+++ packages/backend/apps/user/backend-user-application/src/users/application/handlers/UserCodeCreatedEventHandler.spec.ts
@@ -13,6 +13,9 @@ import { UserCodeCreatedEventHandler } from './UserCodeCreatedEventHandler';
 
 describe(UserCodeCreatedEventHandler.name, () => {
   let mailDeliveryOutputPortMock: jest.Mocked<MailDeliveryOutputPort>;
+  let passwordResetMailDeliveryOptionsFromUserBuilderMock: jest.Mocked<
+    Builder<MailDeliveryOptions, [User, UserCode]>
+  >;
   let userActivationMailDeliveryOptionsFromUserBuilderMock: jest.Mocked<
     Builder<MailDeliveryOptions, [User, UserCode]>
   >;
@@ -23,72 +26,140 @@ describe(UserCodeCreatedEventHandler.name, () => {
     mailDeliveryOutputPortMock = {
       send: jest.fn(),
     };
+    passwordResetMailDeliveryOptionsFromUserBuilderMock = {
+      build: jest.fn(),
+    };
     userActivationMailDeliveryOptionsFromUserBuilderMock = {
       build: jest.fn(),
     };
 
     userCodeCreatedEventHandler = new UserCodeCreatedEventHandler(
       mailDeliveryOutputPortMock,
+      passwordResetMailDeliveryOptionsFromUserBuilderMock,
       userActivationMailDeliveryOptionsFromUserBuilderMock,
     );
   });
 
   describe('.handle', () => {
-    let userCodeCreatedEventFixture: UserCodeCreatedEvent;
+    describe('having a UserCodeCreatedEvent with a non active user', () => {
+      let userCodeCreatedEventFixture: UserCodeCreatedEvent;
 
-    beforeAll(() => {
-      userCodeCreatedEventFixture = UserCodeCreatedEventFixtures.any;
+      beforeAll(() => {
+        userCodeCreatedEventFixture =
+          UserCodeCreatedEventFixtures.withUserWithActiveFalse;
+      });
+
+      describe('when called', () => {
+        let mailDeliveryOptionsFixture: MailDeliveryOptions;
+
+        let result: unknown;
+
+        beforeAll(async () => {
+          mailDeliveryOptionsFixture = {
+            from: 'mail@example.com',
+            subject: 'subject',
+            text: 'sample text',
+            to: ['mail@example.com'],
+          };
+
+          userActivationMailDeliveryOptionsFromUserBuilderMock.build.mockReturnValueOnce(
+            mailDeliveryOptionsFixture,
+          );
+
+          mailDeliveryOutputPortMock.send.mockResolvedValueOnce(undefined);
+
+          result = await userCodeCreatedEventHandler.handle(
+            userCodeCreatedEventFixture,
+          );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call userActivationMailDeliveryOptionsFromUserBuilder.build()', () => {
+          expect(
+            userActivationMailDeliveryOptionsFromUserBuilderMock.build,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            userActivationMailDeliveryOptionsFromUserBuilderMock.build,
+          ).toHaveBeenCalledWith(
+            userCodeCreatedEventFixture.user,
+            userCodeCreatedEventFixture.userCode,
+          );
+        });
+
+        it('should call mailDeliveryOutputPort.send()', () => {
+          expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledTimes(1);
+          expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledWith(
+            mailDeliveryOptionsFixture,
+          );
+        });
+
+        it('should return undefined', () => {
+          expect(result).toBeUndefined();
+        });
+      });
     });
 
-    describe('when called', () => {
-      let mailDeliveryOptionsFixture: MailDeliveryOptions;
+    describe('having a UserCodeCreatedEvent with an active user', () => {
+      let userCodeCreatedEventFixture: UserCodeCreatedEvent;
 
-      let result: unknown;
-
-      beforeAll(async () => {
-        mailDeliveryOptionsFixture = {
-          from: 'mail@example.com',
-          subject: 'subject',
-          text: 'sample text',
-          to: ['mail@example.com'],
-        };
-
-        userActivationMailDeliveryOptionsFromUserBuilderMock.build.mockReturnValueOnce(
-          mailDeliveryOptionsFixture,
-        );
-
-        mailDeliveryOutputPortMock.send.mockResolvedValueOnce(undefined);
-
-        result = await userCodeCreatedEventHandler.handle(
-          userCodeCreatedEventFixture,
-        );
+      beforeAll(() => {
+        userCodeCreatedEventFixture =
+          UserCodeCreatedEventFixtures.withUserWithActiveTrue;
       });
 
-      afterAll(() => {
-        jest.clearAllMocks();
-      });
+      describe('when called', () => {
+        let mailDeliveryOptionsFixture: MailDeliveryOptions;
 
-      it('should call userActivationMailDeliveryOptionsFromUserBuilder.build()', () => {
-        expect(
-          userActivationMailDeliveryOptionsFromUserBuilderMock.build,
-        ).toHaveBeenCalledTimes(1);
-        expect(
-          userActivationMailDeliveryOptionsFromUserBuilderMock.build,
-        ).toHaveBeenCalledWith(
-          userCodeCreatedEventFixture.user,
-          userCodeCreatedEventFixture.userCode,
-        );
-      });
+        let result: unknown;
 
-      it('should call mailDeliveryOutputPort.send()', () => {
-        expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledTimes(1);
-        expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledWith(
-          mailDeliveryOptionsFixture,
-        );
-      });
+        beforeAll(async () => {
+          mailDeliveryOptionsFixture = {
+            from: 'mail@example.com',
+            subject: 'subject',
+            text: 'sample text',
+            to: ['mail@example.com'],
+          };
 
-      it('should return undefined', () => {
-        expect(result).toBeUndefined();
+          passwordResetMailDeliveryOptionsFromUserBuilderMock.build.mockReturnValueOnce(
+            mailDeliveryOptionsFixture,
+          );
+
+          mailDeliveryOutputPortMock.send.mockResolvedValueOnce(undefined);
+
+          result = await userCodeCreatedEventHandler.handle(
+            userCodeCreatedEventFixture,
+          );
+        });
+
+        afterAll(() => {
+          jest.clearAllMocks();
+        });
+
+        it('should call passwordResetMailDeliveryOptionsFromUserBuilder.build()', () => {
+          expect(
+            passwordResetMailDeliveryOptionsFromUserBuilderMock.build,
+          ).toHaveBeenCalledTimes(1);
+          expect(
+            passwordResetMailDeliveryOptionsFromUserBuilderMock.build,
+          ).toHaveBeenCalledWith(
+            userCodeCreatedEventFixture.user,
+            userCodeCreatedEventFixture.userCode,
+          );
+        });
+
+        it('should call mailDeliveryOutputPort.send()', () => {
+          expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledTimes(1);
+          expect(mailDeliveryOutputPortMock.send).toHaveBeenCalledWith(
+            mailDeliveryOptionsFixture,
+          );
+        });
+
+        it('should return undefined', () => {
+          expect(result).toBeUndefined();
+        });
       });
     });
   });
