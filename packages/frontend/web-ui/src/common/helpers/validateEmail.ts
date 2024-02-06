d198887532d3185510a7e022d0d5ffe98fbb67e8
Mario Fernández <mario.fdez.p@gmail.com>
2024-02-06 12:09:24 +0100

    refactor(web-ui): renamed validateEmail module and include Either type

---
+++ packages/frontend/web-ui/src/common/helpers/validateEmail.ts
@@ -0,0 +1,15 @@
+import { Either } from '../models/Either';
+
+export const validateEmail = (email: string): Either<string, undefined> => {
+  if (email.match(/^\S+@\S+$/gi) == null) {
+    return {
+      isRight: false,
+      value: 'The email must be a valid email.',
+    };
+  } else {
+    return {
+      isRight: true,
+      value: undefined,
+    };
+  }
+};
