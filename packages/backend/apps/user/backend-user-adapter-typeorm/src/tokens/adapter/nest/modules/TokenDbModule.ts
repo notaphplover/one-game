93408cbdf28a014dd4c2e28716876e323e10ba05
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-02-10 21:26:34 +0100

    refactor(backend-user-adapter-typeorm): extract TokenDbModule

---
+++ packages/backend/apps/user/backend-user-adapter-typeorm/src/tokens/adapter/nest/modules/TokenDbModule.ts
@@ -0,0 +1,21 @@
+import { DynamicModule, Module } from '@nestjs/common';
+
+import { DbModuleOptions } from '../../../../foundation/db/adapter/nest/models/DbModuleOptions';
+import { DbModule } from '../../../../foundation/db/adapter/nest/modules/DbModule';
+import { RefreshTokenDb } from '../../typeorm/models/RefreshTokenDb';
+
+@Module({})
+export class TokenDbModule {
+  public static forRootAsync(dbModuleOptions: DbModuleOptions): DynamicModule {
+    return {
+      exports: [],
+      global: false,
+      imports: [
+        DbModule.forRootAsync(dbModuleOptions),
+        dbModuleOptions.builders.feature([RefreshTokenDb]),
+      ],
+      module: TokenDbModule,
+      providers: [],
+    };
+  }
+}
