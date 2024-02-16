57e25d4482a5c81a8595e6708d7f71a5f24e4f71
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-02-16 22:02:29 +0100

    refactor(backend-user-application): update RefreshTokenPersistenceOutputPort with findOne

--- packages/backend/apps/user/backend-user-domain/src/tokens/domain/fixtures/RefreshTokenFindQueryFixtures.ts
+++ packages/backend/apps/user/backend-user-domain/src/tokens/domain/fixtures/RefreshTokenFindQueryFixtures.ts
@@ -1,6 +1,12 @@
 import { RefreshTokenFindQuery } from '../queries/RefreshTokenFindQuery';
 
 export class RefreshTokenFindQueryFixtures {
+  public static get any(): RefreshTokenFindQuery {
+    return {
+      id: 'f3073aec-b81b-4107-97f9-baa46de5d441',
+    };
+  }
+
   public static get withId(): RefreshTokenFindQuery {
     return {
       id: 'f3073aec-b81b-4107-97f9-baa46de5d441',
