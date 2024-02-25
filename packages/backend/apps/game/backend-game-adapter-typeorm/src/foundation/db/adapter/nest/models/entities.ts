3b0ed9540790f7742b796a704cb12dfb47ca4822
notaphplover <roberto.pintos.lopez@gmail.com>
2024-02-25 14:39:43 +0100

    refactor(backend-game-adapter-typeorm): move GameInitialSnapshotDb models

--- packages/backend/apps/game/backend-game-adapter-typeorm/src/foundation/db/adapter/nest/models/entities.ts
+++ packages/backend/apps/game/backend-game-adapter-typeorm/src/foundation/db/adapter/nest/models/entities.ts
@@ -1,7 +1,7 @@
+import { GameInitialSnapshotDb } from '../../../../../gameInitialSnapshots/adapter/typeorm/models/GameInitialSnapshotDb';
+import { GameInitialSnapshotSlotDb } from '../../../../../gameInitialSnapshots/adapter/typeorm/models/GameInitialSnapshotSlotDb';
 import { GameActionDb } from '../../../../../games/adapter/typeorm/models/GameActionDb';
 import { GameDb } from '../../../../../games/adapter/typeorm/models/GameDb';
-import { GameInitialSnapshotDb } from '../../../../../games/adapter/typeorm/models/GameInitialSnapshotDb';
-import { GameInitialSnapshotSlotDb } from '../../../../../games/adapter/typeorm/models/GameInitialSnapshotSlotDb';
 import { GameSlotDb } from '../../../../../games/adapter/typeorm/models/GameSlotDb';
 import { GameSpecDb } from '../../../../../games/adapter/typeorm/models/GameSpecDb';
 
