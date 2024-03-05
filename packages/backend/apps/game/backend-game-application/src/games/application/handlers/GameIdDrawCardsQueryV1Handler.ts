3ab06c655a1fde87995fd1a9bec3ab43c26c44e4
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-03-05 22:21:53 +0100

    refactor(backend-game-application): add GameIdDrawCardsQueryV1Handler

---
+++ packages/backend/apps/game/backend-game-application/src/games/application/handlers/GameIdDrawCardsQueryV1Handler.ts
@@ -0,0 +1,89 @@
+import { models as apiModels } from '@cornie-js/api-models';
+import {
+  AppError,
+  AppErrorKind,
+  Builder,
+  Handler,
+} from '@cornie-js/backend-common';
+import {
+  ActiveGame,
+  GameDrawCardsUpdateQueryFromGameBuilder,
+  GameSpec,
+  GameUpdateQuery,
+  PlayerCanDrawCardsSpec,
+  PlayerCanUpdateGameSpec,
+} from '@cornie-js/backend-game-domain/games';
+import { Inject, Injectable } from '@nestjs/common';
+
+import { GameUpdatedEvent } from '../models/GameUpdatedEvent';
+import {
+  GamePersistenceOutputPort,
+  gamePersistenceOutputPortSymbol,
+} from '../ports/output/GamePersistenceOutputPort';
+import {
+  GameSpecPersistenceOutputPort,
+  gameSpecPersistenceOutputPortSymbol,
+} from '../ports/output/GameSpecPersistenceOutputPort';
+import { GameIdUpdateQueryV1Handler } from './GameIdUpdateQueryV1Handler';
+import { GameUpdatedEventHandler } from './GameUpdatedEventHandler';
+
+@Injectable()
+export class GameIdDrawCardsQueryV1Handler extends GameIdUpdateQueryV1Handler<apiModels.GameIdDrawCardsQueryV1> {
+  readonly #gameDrawCardsUpdateQueryFromGameBuilder: Builder<
+    GameUpdateQuery,
+    [ActiveGame]
+  >;
+  readonly #playerCanDrawCardsSpec: PlayerCanDrawCardsSpec;
+
+  constructor(
+    @Inject(GameDrawCardsUpdateQueryFromGameBuilder)
+    gameDrawCardsUpdateQueryFromGameBuilder: Builder<
+      GameUpdateQuery,
+      [ActiveGame]
+    >,
+    @Inject(gamePersistenceOutputPortSymbol)
+    gamePersistenceOutputPort: GamePersistenceOutputPort,
+    @Inject(gameSpecPersistenceOutputPortSymbol)
+    gameSpecPersistenceOutputPort: GameSpecPersistenceOutputPort,
+    @Inject(GameUpdatedEventHandler)
+    gameUpdatedEventHandler: Handler<[GameUpdatedEvent], void>,
+    @Inject(PlayerCanUpdateGameSpec)
+    playerCanUpdateGameSpec: PlayerCanUpdateGameSpec,
+    @Inject(PlayerCanDrawCardsSpec)
+    playerCanDrawCardsSpec: PlayerCanDrawCardsSpec,
+  ) {
+    super(
+      gameSpecPersistenceOutputPort,
+      gamePersistenceOutputPort,
+      gameUpdatedEventHandler,
+      playerCanUpdateGameSpec,
+    );
+
+    this.#gameDrawCardsUpdateQueryFromGameBuilder =
+      gameDrawCardsUpdateQueryFromGameBuilder;
+    this.#playerCanDrawCardsSpec = playerCanDrawCardsSpec;
+  }
+
+  protected override _buildUpdateQuery(game: ActiveGame): GameUpdateQuery {
+    return this.#gameDrawCardsUpdateQueryFromGameBuilder.build(game);
+  }
+
+  protected override _checkUnprocessableOperation(
+    game: ActiveGame,
+    gameSpec: GameSpec,
+    gameIdPassTurnQueryV1: apiModels.GameIdDrawCardsQueryV1,
+  ): void {
+    if (
+      !this.#playerCanDrawCardsSpec.isSatisfiedBy(
+        game,
+        gameSpec.options,
+        gameIdPassTurnQueryV1.slotIndex,
+      )
+    ) {
+      throw new AppError(
+        AppErrorKind.unprocessableOperation,
+        'Player cannot end the turn. Reason: there is a pending action preventing the turn to be ended',
+      );
+    }
+  }
+}
