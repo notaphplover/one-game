c23d882fbc2c81ce16d52c317c6a1d0487571ed2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-03-07 20:47:14 +0100

    refactor(backend-game-application): update NonStartedGameFilledEventHandler update handler to rely on startGameUpdateQueryFromGameBuilder

--- packages/backend/apps/game/backend-game-domain/src/games/domain/services/GameService.spec.ts
+++ packages/backend/apps/game/backend-game-domain/src/games/domain/services/GameService.spec.ts
@@ -1,111 +1,87 @@
-import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+import { beforeAll, describe, expect, it, jest } from '@jest/globals';
 
-import { AreCardsEqualsSpec } from '../../../cards/domain/specs/AreCardsEqualsSpec';
-import { Card } from '../../../cards/domain/valueObjects/Card';
-import { CardColor } from '../../../cards/domain/valueObjects/CardColor';
+import { AppError, AppErrorKind } from '@cornie-js/backend-common';
+
 import { NonStartedGame } from '../entities/NonStartedGame';
-import { GameSpecFixtures } from '../fixtures';
-import { GameInitialDrawsMutationFixtures } from '../fixtures/GameInitialDrawsMutationFixtures';
 import { NonStartedGameFixtures } from '../fixtures/NonStartedGameFixtures';
-import { GameUpdateQuery } from '../query/GameUpdateQuery';
-import { GameDirection } from '../valueObjects/GameDirection';
-import { GameInitialDrawsMutation } from '../valueObjects/GameInitialDrawsMutation';
-import { GameSpec } from '../valueObjects/GameSpec';
-import { GameStatus } from '../valueObjects/GameStatus';
 import { GameDrawService } from './GameDrawService';
 import { GameService } from './GameService';
 
 describe(GameService.name, () => {
-  let areCardsEqualsSpecMock: jest.Mocked<AreCardsEqualsSpec>;
   let gameDrawServiceMock: jest.Mocked<GameDrawService>;
 
   let gameService: GameService;
 
   beforeAll(() => {
-    areCardsEqualsSpecMock = {
-      isSatisfiedBy: jest.fn(),
-    } as Partial<
-      jest.Mocked<AreCardsEqualsSpec>
-    > as jest.Mocked<AreCardsEqualsSpec>;
-
     gameDrawServiceMock = {
       calculateDrawMutation: jest.fn(),
       calculateInitialCardsDrawMutation: jest.fn(),
     } as Partial<jest.Mocked<GameDrawService>> as jest.Mocked<GameDrawService>;
 
-    gameService = new GameService(areCardsEqualsSpecMock, gameDrawServiceMock);
+    gameService = new GameService(gameDrawServiceMock);
   });
 
-  describe('.buildStartGameUpdateQuery', () => {
-    describe('having a Game', () => {
+  describe('.getGameSlotOrThrow', () => {
+    describe('having a game with a single game slot', () => {
       let gameFixture: NonStartedGame;
-      let gameSpecFixture: GameSpec;
 
       beforeAll(() => {
         gameFixture = NonStartedGameFixtures.withGameSlotsOne;
-        gameSpecFixture = GameSpecFixtures.any;
       });
 
-      describe('when called', () => {
-        let gameInitialDrawsMutationFixture: GameInitialDrawsMutation;
-        let result: unknown;
+      describe('having an existing index', () => {
+        let slotIndexFixture: number;
 
         beforeAll(() => {
-          gameInitialDrawsMutationFixture =
-            GameInitialDrawsMutationFixtures.withCardsOneCardArray;
-
-          gameDrawServiceMock.calculateInitialCardsDrawMutation.mockReturnValueOnce(
-            gameInitialDrawsMutationFixture,
-          );
-
-          result = gameService.buildStartGameUpdateQuery(
-            gameFixture,
-            gameSpecFixture,
-          );
+          slotIndexFixture = 0;
         });
 
-        afterAll(() => {
-          jest.clearAllMocks();
+        describe('when called', () => {
+          let result: unknown;
+
+          beforeAll(() => {
+            result = gameService.getGameSlotOrThrow(
+              gameFixture,
+              slotIndexFixture,
+            );
+          });
+
+          it('should return a GameSlot', () => {
+            expect(result).toBe(gameFixture.state.slots[0]);
+          });
+        });
+      });
+
+      describe('having a non existing index', () => {
+        let slotIndexFixture: number;
+
+        beforeAll(() => {
+          slotIndexFixture = -1;
         });
 
-        it('should call gameDrawService.calculateInitialCardsDrawMutation()', () => {
-          expect(
-            gameDrawServiceMock.calculateInitialCardsDrawMutation,
-          ).toHaveBeenCalledTimes(1);
-          expect(
-            gameDrawServiceMock.calculateInitialCardsDrawMutation,
-          ).toHaveBeenCalledWith(gameSpecFixture);
-        });
+        describe('when called', () => {
+          let result: unknown;
 
-        it('should return a GameUpdateQuery', () => {
-          const expectedGameUpdateQueryProperties: Partial<GameUpdateQuery> = {
-            currentCard: gameInitialDrawsMutationFixture.currentCard,
-            currentColor: expect.any(String) as unknown as CardColor,
-            currentDirection: GameDirection.clockwise,
-            currentPlayingSlotIndex: 0,
-            currentTurnCardsPlayed: false,
-            deck: gameInitialDrawsMutationFixture.deck,
-            drawCount: 0,
-            gameFindQuery: {
-              id: gameFixture.id,
-            },
-            gameSlotUpdateQueries: [
-              {
-                cards: gameInitialDrawsMutationFixture.cards[0] as Card[],
-                gameSlotFindQuery: {
-                  gameId: gameFixture.id,
-                  position: 0,
-                },
-              },
-            ],
-            skipCount: 0,
-            status: GameStatus.active,
-            turn: 1,
-          };
+          beforeAll(() => {
+            try {
+              gameService.getGameSlotOrThrow(gameFixture, slotIndexFixture);
+            } catch (error: unknown) {
+              result = error;
+            }
+          });
 
-          expect(result).toStrictEqual(
-            expect.objectContaining(expectedGameUpdateQueryProperties),
-          );
+          it('should throw an Error', () => {
+            const expectedErrorProperties: Partial<AppError> = {
+              kind: AppErrorKind.unknown,
+              message:
+                'Expecting a game slot at index "-1", none found instead.',
+            };
+
+            expect(result).toBeInstanceOf(AppError);
+            expect(result).toStrictEqual(
+              expect.objectContaining(expectedErrorProperties),
+            );
+          });
         });
       });
     });
