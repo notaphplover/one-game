ab09534ff331cd4af46f68a402c68c858a9c4b8f
Mario Fernández <mario.fdez.p@gmail.com>
2024-03-11 18:14:50 +0100

    refactor(web-ui): changes to transform CornieHome and his test from JSX to TSX

--- packages/frontend/web-ui/src/home/pages/CornieHome.tsx
+++ packages/frontend/web-ui/src/home/pages/CornieHome.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Home } from '../components/Home';
 import { HomeWithAuth } from '../components/HomeWithAuth';
 
-export const CornieHome = () => {
-  const { token } = useSelector((state) => state.auth);
+export const CornieHome = (): React.JSX.Element => {
+  const { token } = useSelector((state: any) => state.auth);
 
   if (token === null) {
     return <Home />;
