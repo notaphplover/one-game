7eefd639265a0995cb88f2a00fe8d9af48856ee4
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-03-12 18:35:57 +0100

    feat(backend-game-application): add GameActionPersistenceOutputPort

---
+++ packages/backend/apps/game/backend-game-application/src/gameActions/application/index.ts
@@ -0,0 +1,8 @@
+import {
+  GameActionPersistenceOutputPort,
+  gameActionPersistenceOutputPortSymbol,
+} from './ports/output/GameActionPersistenceOutputPort';
+
+export type { GameActionPersistenceOutputPort };
+
+export { gameActionPersistenceOutputPortSymbol };
