35a1ca616d467cca070d1d989bef577c1e39294f
Mario Fernández <mario.fdez.p@gmail.com>
2024-03-12 16:20:49 +0100

    refactor(web-ui): add new variable to select token in authSlice and change on CornieHome

--- packages/frontend/web-ui/src/home/pages/CornieHome.tsx
+++ packages/frontend/web-ui/src/home/pages/CornieHome.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Home } from '../components/Home';
 import { HomeWithAuth } from '../components/HomeWithAuth';
+import { useAppSelector } from '../../app/store/hooks';
+import { selectAuthToken } from '../../app/store/features/authSlice';
 
 export const CornieHome = (): React.JSX.Element => {
-  const { token } = useSelector((state: any) => state.auth);
+  const { token } = useAppSelector(selectAuthToken);
 
   if (token === null) {
     return <Home />;
