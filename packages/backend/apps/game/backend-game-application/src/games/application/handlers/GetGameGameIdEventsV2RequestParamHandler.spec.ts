10ac869709fe6d9026a0ed3aa8d4e1bf17eac6d9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-03-21 00:41:03 +0100

    refactor(backend-game-application): add GetGameGameIdEventsV2RequestParamHandler

---
+++ packages/backend/apps/game/backend-game-application/src/games/application/handlers/GetGameGameIdEventsV2RequestParamHandler.spec.ts
@@ -0,0 +1,173 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { models as apiModels } from '@cornie-js/api-models';
+import { AppError, AppErrorKind } from '@cornie-js/backend-common';
+import { Game } from '@cornie-js/backend-game-domain/games';
+import { NonStartedGameFixtures } from '@cornie-js/backend-game-domain/games/fixtures';
+import {
+  AuthKind,
+  AuthRequestContextHolder,
+  Request,
+  requestContextProperty,
+} from '@cornie-js/backend-http';
+
+import { UserV1Fixtures } from '../../../users/application/fixtures/models/UserV1Fixtures';
+import { GameRequestContextHolder } from '../models/GameRequestContextHolder';
+import { GetGameGameIdEventsV2RequestParamHandler } from './GetGameGameIdEventsV2RequestParamHandler';
+
+describe(GetGameGameIdEventsV2RequestParamHandler.name, () => {
+  let getGameGameIdEventsV2RequestParamHandler: GetGameGameIdEventsV2RequestParamHandler;
+
+  beforeAll(() => {
+    getGameGameIdEventsV2RequestParamHandler =
+      new GetGameGameIdEventsV2RequestParamHandler();
+  });
+
+  describe('.handle', () => {
+    describe('having a Request with no last-event-id header, a stringified number gameSlotId param and service auth', () => {
+      let gameFixture: Game;
+      let requestFixture: Request &
+        AuthRequestContextHolder &
+        GameRequestContextHolder;
+
+      beforeAll(() => {
+        gameFixture = NonStartedGameFixtures.any;
+        requestFixture = {
+          headers: {},
+          query: {},
+          [requestContextProperty]: {
+            auth: {
+              kind: AuthKind.backendService,
+            },
+            game: gameFixture,
+          },
+          urlParameters: {},
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(async () => {
+          try {
+            await getGameGameIdEventsV2RequestParamHandler.handle(
+              requestFixture,
+            );
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        it('should throw an Error', () => {
+          const expectedErrorProperties: Partial<AppError> = {
+            kind: AppErrorKind.invalidCredentials,
+            message: 'Expecting user based credentials',
+          };
+
+          expect(result).toBeInstanceOf(AppError);
+          expect(result).toStrictEqual(
+            expect.objectContaining(expectedErrorProperties),
+          );
+        });
+      });
+    });
+
+    describe('having a Request with no last-event-id header, a stringified number gameSlotId param and user auth', () => {
+      let gameFixture: Game;
+      let userFixture: apiModels.UserV1;
+      let requestFixture: Request &
+        AuthRequestContextHolder &
+        GameRequestContextHolder;
+
+      beforeAll(() => {
+        gameFixture = NonStartedGameFixtures.any;
+        userFixture = UserV1Fixtures.any;
+        requestFixture = {
+          headers: {},
+          query: {},
+          [requestContextProperty]: {
+            auth: {
+              jwtPayload: {
+                [Symbol()]: Symbol(),
+              },
+              kind: AuthKind.user,
+              user: userFixture,
+            },
+            game: gameFixture,
+          },
+          urlParameters: {},
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(async () => {
+          result =
+            await getGameGameIdEventsV2RequestParamHandler.handle(
+              requestFixture,
+            );
+        });
+
+        it('should return a number and a game', () => {
+          expect(result).toStrictEqual([
+            requestFixture[requestContextProperty].game,
+            userFixture,
+            null,
+          ]);
+        });
+      });
+    });
+
+    describe('having a Request with last-event-id header, a stringified number gameSlotId param and user auth', () => {
+      let gameFixture: Game;
+      let userFixture: apiModels.UserV1;
+      let lastEventIdFixture: string;
+      let requestFixture: Request &
+        AuthRequestContextHolder &
+        GameRequestContextHolder;
+
+      beforeAll(() => {
+        gameFixture = NonStartedGameFixtures.any;
+        userFixture = UserV1Fixtures.any;
+        lastEventIdFixture = 'last-event-id-fixture';
+        requestFixture = {
+          headers: {
+            'last-event-id': lastEventIdFixture,
+          },
+          query: {},
+          [requestContextProperty]: {
+            auth: {
+              jwtPayload: {
+                [Symbol()]: Symbol(),
+              },
+              kind: AuthKind.user,
+              user: userFixture,
+            },
+            game: gameFixture,
+          },
+          urlParameters: {},
+        };
+      });
+
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(async () => {
+          result =
+            await getGameGameIdEventsV2RequestParamHandler.handle(
+              requestFixture,
+            );
+        });
+
+        it('should return a number and a game', () => {
+          expect(result).toStrictEqual([
+            requestFixture[requestContextProperty].game,
+            userFixture,
+            lastEventIdFixture,
+          ]);
+        });
+      });
+    });
+  });
+});
