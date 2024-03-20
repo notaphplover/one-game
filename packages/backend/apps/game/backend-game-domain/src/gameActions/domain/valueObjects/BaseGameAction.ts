8f602249a8da5c69e83e7b04c53ce56f2984b79f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-03-20 19:01:39 +0100

    refactor(backend-game-adapter-typeorm): add GameActionFromGameActionDbBuilder

--- packages/backend/apps/game/backend-game-domain/src/gameActions/domain/valueObjects/BaseGameAction.ts
+++ packages/backend/apps/game/backend-game-domain/src/gameActions/domain/valueObjects/BaseGameAction.ts
@@ -3,6 +3,7 @@ import { GameActionKind } from './GameActionKind';
 export interface BaseGameAction<TKind extends GameActionKind> {
   readonly currentPlayingSlotIndex: number;
   readonly gameId: string;
+  readonly id: string;
   readonly kind: TKind;
   readonly position: number;
   readonly turn: number;
