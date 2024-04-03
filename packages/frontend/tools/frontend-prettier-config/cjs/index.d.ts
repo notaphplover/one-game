55a09b049f18d44cf435f0bd011fbb50e2df9834
notaphplover <roberto.pintos.lopez@gmail.com>
2024-04-04 00:40:47 +0200

    chore: add frontend-prettier-config package

---
+++ packages/frontend/tools/frontend-prettier-config/cjs/index.d.ts
@@ -0,0 +1,9 @@
+export const printWidth: number;
+export const tabWidth: number;
+export const useTabs: boolean;
+export const semi: boolean;
+export const singleQuote: boolean;
+export const bracketSpacing: boolean;
+export const arrowParens: "avoid" | "always";
+export const endOfLine: "auto" | "lf" | "crlf" | "cr";
+export const trailingComma: "none" | "es5" | "all";
