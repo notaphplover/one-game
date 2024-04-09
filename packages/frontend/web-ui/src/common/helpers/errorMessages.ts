4affbd855bf741a8d1482957cff1a1e24d1e5a1c
Mario Fernández <mario.fdez.p@gmail.com>
2024-04-09 17:51:03 +0200

    feat(web-ui): add new files for errorMessage, constants and validation

---
+++ packages/frontend/web-ui/src/common/helpers/errorMessages.ts
@@ -0,0 +1,9 @@
+export const UNAUTHORIZED_ERROR_MESSAGE: string = 'Unauthorized error.';
+export const HTTP_BAD_REQUEST_ERROR_MESSAGE: string =
+  'Unexpected error occurred while processing the request.';
+export const FORBIDDEN_ERROR_MESSAGE: string = `Forbidden error.`;
+export const INVALID_CREDENTIALS_ERROR_MESSAGE: string = 'Invalid credentials.';
+export const HTTP_CONFLICT_ERROR_MESSAGE: string =
+  "An error has occurred. You can't join it in this game.";
+export const UNPROCESSABLE_REQUEST_ERROR_MESSAGE: string =
+  'Unprocesable request.';
