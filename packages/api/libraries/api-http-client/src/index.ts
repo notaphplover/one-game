d6be96c923f04b67e0056ea387c1abe444c227df
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-04-14 11:58:13 +0200

    feat(api-http-client): expose models

--- packages/api/libraries/api-http-client/src/index.ts
+++ packages/api/libraries/api-http-client/src/index.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from './httpClient/generated/HttpClient';
+import { Interceptor } from './httpClient/models/Interceptor';
+import { Request } from './httpClient/models/Request';
 import { Response } from './httpClient/models/Response';
 
-export type { Response };
+export type { Interceptor, Request, Response };
 
 export { HttpClient };
