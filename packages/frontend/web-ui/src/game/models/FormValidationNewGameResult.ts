7ca9ed12b06471671e5afc7a4ec98911eb3a1077
Mario Fernández <mario.fdez.p@gmail.com>
2024-04-15 15:44:59 +0200

    refactor(web-ui): delete deprecated files

--- packages/frontend/web-ui/src/game/models/FormValidationNewGameResult.ts
+++
@@ -1,3 +0,0 @@
-export interface FormValidationNewGameResult {
-  numberOfPlayers?: string;
-}
--- packages/frontend/web-ui/src/game/models/GameOptions.ts
+++
@@ -1,9 +0,0 @@
-export interface GameOptions {
-  chainDraw2Draw2Cards: boolean;
-  chainDraw2Draw4Cards: boolean;
-  chainDraw4Draw4Cards: boolean;
-  chainDraw4Draw2Cards: boolean;
-  playCardIsMandatory: boolean;
-  playMultipleSameCards: boolean;
-  playWildDraw4IfNoOtherAlternative: boolean;
-}
