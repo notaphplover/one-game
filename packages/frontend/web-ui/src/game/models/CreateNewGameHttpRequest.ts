7cd0a802518088bb8262ca74f551b3ccd0bd9202
Mario Fernández <mario.fdez.p@gmail.com>
2024-04-16 12:49:13 +0200

    refactor(web-ui): delete interface

--- packages/frontend/web-ui/src/game/models/CreateNewGameHttpRequest.ts
+++
@@ -1,7 +0,0 @@
-import { GameOptionsV1 } from '@cornie-js/api-models/lib/models/types';
-
-export interface CreateNewGameHttpRequest {
-  gameSlotsAmount: number;
-  name?: string;
-  options: GameOptionsV1;
-}
