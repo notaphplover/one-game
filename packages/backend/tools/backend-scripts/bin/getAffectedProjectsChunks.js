d3b71dd4188289fa99cb1f522b6be5212652e2fe
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-05-05 21:35:45 +0200

    chore: update ci to group tests in chunks

--- packages/backend/tools/backend-scripts/bin/getAffectedProjectsChunks.js
+++ packages/backend/tools/backend-scripts/bin/getAffectedProjectsChunks.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 
-import { argv } from 'node:process';
+import process from 'node:process';
 import { promisifiedExec } from '../src/promisifiedExec.js';
+
+/**
+ * @param {Array} elements Elements
+ * @param {number} chunks chunks amount
+ * @returns {Array.<Array>}
+ */
+function buildChunks(elements, chunks) {
+  const chunkSize = Math.ceil(elements.length / chunks);
+
+  const chunksArray = [];
+
+  for (let i = 0; i < elements.length; i += chunkSize) {
+    chunksArray.push(elements.slice(i, i + chunkSize));
+  }
+
+  return chunksArray;
+}
 
 const TURBOREPO_ROOT_PACKAGE_NAME = '//';
 const TURBOREPO_TASK_NOT_FOUND_MAGIC_STRING = '\u003cNONEXISTENT\u003e';
 
-const taskToDryRun = argv[2];
-const baseRef = argv[3];
+const taskToDryRun = process.argv[2];
+const baseRef = process.argv[3];
+const chunks = parseInt(process.argv[4]);
 
 let execCommand = `pnpm exec turbo run ${taskToDryRun} --dry=json`;
 
@@ -35,4 +53,8 @@ const tasks = dryRunResult.tasks.filter((task) =>
 /** @type {Array.<string>} */
 const packageNames = tasks.map((task) => task.package);
 
-console.log(JSON.stringify(packageNames));
+const packageNameChunks = buildChunks(packageNames, chunks).filter(
+  (chunk) => chunk.length > 0,
+);
+
+process.stdout.write(JSON.stringify(packageNameChunks));
