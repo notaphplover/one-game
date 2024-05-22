e114b4564b96cb61dfaa01e4111d1c7a182054f8
Mario Fernández <mario.fdez.p@gmail.com>
2024-05-22 16:13:34 +0200

    feat(web-ui): add new hook useJoinGame using useSingleAuthorizedApiCall

---
+++ packages/frontend/web-ui/src/game/hooks/useJoinGame/utils/unexpectedErrorMessage.ts
@@ -0,0 +1,10 @@
+export const UNEXPECTED_ERROR_MESSAGE: string =
+  'Unexpected error when fetching user games';
+export const UNAUTHORIZED_ERROR_MESSAGE: string = 'Unauthorized error.';
+export const HTTP_BAD_REQUEST_ERROR_MESSAGE: string =
+  'Unexpected error occurred while processing the request.';
+export const FORBIDDEN_ERROR_MESSAGE: string = `Forbidden error.`;
+export const CONFLICT_ERROR_MESSAGE: string =
+  'Conflict error. You can join this game.';
+export const UNPROCESSABLE_REQUEST_ERROR_MESSAGE: string =
+  'Unprocessable request error.';
