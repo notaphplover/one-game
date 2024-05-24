2d92a744a42f43abf8ddb081b5ff2604a2d9f3f9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-05-25 00:40:55 +0200

    chore: add frontend-common package

---
+++ packages/frontend/libraries/frontend-common/src/error/models/AppErrorKind.ts
@@ -0,0 +1,9 @@
+export enum AppErrorKind {
+  contractViolation = 'contractViolation',
+  entityConflict = 'entityConflict',
+  entityNotFound = 'entityNotFound',
+  invalidCredentials = 'invalidCredentials',
+  missingCredentials = 'missingCredentials',
+  unknown = 'unknown',
+  unprocessableOperation = 'unprocessableOperation',
+}
