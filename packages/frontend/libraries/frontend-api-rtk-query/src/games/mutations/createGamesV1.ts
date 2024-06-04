8d38e6c931d7305cdfabee3c9a9f77b25ef988b2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-06-04 22:04:45 +0200

    refactor(frontend-api-rtk-query): add createGamesV1

---
+++ packages/frontend/libraries/frontend-api-rtk-query/src/games/mutations/createGamesV1.ts
@@ -0,0 +1,75 @@
+import { HttpClient } from '@cornie-js/api-http-client';
+import { models as apiModels } from '@cornie-js/api-models';
+import { AppError, AppErrorKind } from '@cornie-js/frontend-common';
+import { BaseQueryApi } from '@reduxjs/toolkit/query';
+
+import { SerializableAppError } from '../../foundation/error/SerializableAppError';
+import { HttpApiResult } from '../../foundation/http/models/HttpApiResult';
+import {
+  BAD_REQUEST,
+  FORBIDDEN,
+  OK,
+  UNAUTHORIZED,
+} from '../../foundation/http/models/httpCodes';
+import { QueryReturnValue } from '../../foundation/http/models/QueryReturnValue';
+import { CreateGamesV1Args } from '../models/CreateGamesV1Args';
+
+type CreateGamesV1Result = HttpApiResult<'createGame'>;
+
+export function createGamesV1(
+  httpClient: HttpClient,
+): (
+  args: CreateGamesV1Args,
+  api: BaseQueryApi,
+  accessToken: string | null,
+) => Promise<
+  QueryReturnValue<apiModels.NonStartedGameV1, SerializableAppError, never>
+> {
+  return async (
+    args: CreateGamesV1Args,
+    _api: BaseQueryApi,
+    accessToken: string | null,
+  ): Promise<
+    QueryReturnValue<apiModels.NonStartedGameV1, SerializableAppError, never>
+  > => {
+    const httpResponse: CreateGamesV1Result =
+      await httpClient.endpoints.createGame(
+        {
+          authorization: `Bearer ${accessToken}`,
+        },
+        ...args.params,
+      );
+
+    switch (httpResponse.statusCode) {
+      case OK:
+        return {
+          data: httpResponse.body,
+        };
+      case BAD_REQUEST:
+        return {
+          error: {
+            kind: AppErrorKind.contractViolation,
+            message: httpResponse.body.description,
+          },
+        };
+      case UNAUTHORIZED:
+        return {
+          error: {
+            kind: AppErrorKind.missingCredentials,
+            message: httpResponse.body.description,
+          },
+        };
+      case FORBIDDEN:
+        return {
+          error: {
+            kind: AppErrorKind.invalidCredentials,
+            message: httpResponse.body.description,
+          },
+        };
+      default:
+        return {
+          error: new AppError(AppErrorKind.unknown),
+        };
+    }
+  };
+}
