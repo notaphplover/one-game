ef27b133a94db6ed18818a2bd0eb1265f86469ba
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-06-09 20:47:20 +0200

    test(web-ui): update cornieApi mock with additional properties

--- packages/frontend/web-ui/src/common/http/services/__mocks__/cornieApi.ts
+++ packages/frontend/web-ui/src/common/http/services/__mocks__/cornieApi.ts
@@ -4,7 +4,10 @@ import type { cornieApi as originalCornieApi } from '../cornieApi';
 
 export const cornieApi: jest.Mocked<typeof originalCornieApi> = {
   useCreateAuthV2Mutation: jest.fn(),
+  useCreateGamesV1Mutation: jest.fn(),
+  useCreateGamesV1SlotsMutation: jest.fn(),
   useGetGamesV1MineQuery: jest.fn(),
+  useGetUsersV1MeQuery: jest.fn(),
 } as Partial<jest.Mocked<typeof originalCornieApi>> as jest.Mocked<
   typeof originalCornieApi
 >;
