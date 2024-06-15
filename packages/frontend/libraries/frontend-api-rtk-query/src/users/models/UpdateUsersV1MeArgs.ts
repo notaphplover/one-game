a6cb8802af8531e8987df08745648081ff33513e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-06-15 17:37:36 +0200

    feat(frontend-api-rtk-query): update cornieApi with updateUsersV1Me mutation

---
+++ packages/frontend/libraries/frontend-api-rtk-query/src/users/models/UpdateUsersV1MeArgs.ts
@@ -0,0 +1,5 @@
+import { HttpApiParamsWithNoHeaders } from '../../foundation/http/models/HttpApiParamsWithNoHeaders';
+
+export interface UpdateUsersV1MeArgs {
+  params: HttpApiParamsWithNoHeaders<'updateUserMe'>;
+}
---
+++ packages/frontend/libraries/frontend-api-rtk-query/src/users/mutations/updateUsersV1Me.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@cornie-js/api-http-client';
+import { models as apiModels } from '@cornie-js/api-models';
+import { AppError, AppErrorKind } from '@cornie-js/frontend-common';
+import { BaseQueryApi } from '@reduxjs/toolkit/query';
+
+import { SerializableAppError } from '../../foundation/error/SerializableAppError';
+import { HttpApiResult } from '../../foundation/http/models/HttpApiResult';
+import {
+  FORBIDDEN,
+  OK,
+  UNAUTHORIZED,
+} from '../../foundation/http/models/httpCodes';
+import { QueryReturnValue } from '../../foundation/http/models/QueryReturnValue';
+import { UpdateUsersV1MeArgs } from '../models/UpdateUsersV1MeArgs';
+
+type UpdateUsersV1MeResult = HttpApiResult<'updateUserMe'>;
+
+export function updateUsersV1Me(
+  httpClient: HttpClient,
+): (
+  args: UpdateUsersV1MeArgs,
+  api: BaseQueryApi,
+  accessToken: string | null,
+) => Promise<QueryReturnValue<apiModels.UserV1, SerializableAppError, never>> {
+  return async (
+    args: UpdateUsersV1MeArgs,
+    _api: BaseQueryApi,
+    accessToken: string | null,
+  ): Promise<
+    QueryReturnValue<apiModels.UserV1, SerializableAppError, never>
+  > => {
+    const httpResponse: UpdateUsersV1MeResult =
+      await httpClient.endpoints.updateUserMe(
+        {
+          authorization: `Bearer ${accessToken}`,
+        },
+        ...args.params,
+      );
+
+    switch (httpResponse.statusCode) {
+      case OK:
+        return {
+          data: httpResponse.body,
+        };
+      case UNAUTHORIZED:
+        return {
+          error: {
+            kind: AppErrorKind.missingCredentials,
+            message: httpResponse.body.description,
+          },
+        };
+      case FORBIDDEN:
+        return {
+          error: {
+            kind: AppErrorKind.invalidCredentials,
+            message: httpResponse.body.description,
+          },
+        };
+      default:
+        return {
+          error: new AppError(AppErrorKind.unknown),
+        };
+    }
+  };
+}
