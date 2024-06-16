7159bc63fd851fb068880bde1affbf7cf82ce44a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-06-16 11:07:03 +0200

    refactor(web-ui): add mapUseQueryHookResult

---
+++ packages/frontend/web-ui/src/common/helpers/mapUseQueryHookResult.spec.ts
@@ -0,0 +1,79 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { Either } from '../models/Either';
+import {
+  UseQueryStateResult,
+  mapUseQueryHookResult,
+} from './mapUseQueryHookResult';
+
+describe(mapUseQueryHookResult.name, () => {
+  describe.each<
+    [string, UseQueryStateResult<unknown>, Either<string, unknown> | null]
+  >([
+    [
+      'with isLoading true',
+      {
+        error: undefined,
+        isLoading: true,
+      },
+      null,
+    ],
+    [
+      'with isLoading false and result.data undefined and error undefined',
+      {
+        data: undefined,
+        error: undefined,
+        isLoading: false,
+      },
+      {
+        isRight: false,
+        value: '',
+      },
+    ],
+    [
+      'with isLoading false and result.data undefined and error with string',
+      {
+        data: undefined,
+        error: {
+          message: 'message-fixture',
+        },
+        isLoading: false,
+      },
+      {
+        isRight: false,
+        value: 'message-fixture',
+      },
+    ],
+    [
+      'with isLoading false and result.data not undefined',
+      {
+        data: { foo: 'bar' },
+        error: undefined,
+        isLoading: false,
+      },
+      {
+        isRight: true,
+        value: { foo: 'bar' },
+      },
+    ],
+  ])(
+    'having a UseQueryStateResult %s',
+    (
+      _: string,
+      useQueryStateResultFixture: UseQueryStateResult<unknown>,
+      expectedResult: Either<string, unknown> | null,
+    ) => {
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = mapUseQueryHookResult(useQueryStateResultFixture);
+        });
+
+        it('should return expected result', () => {
+          expect(result).toStrictEqual(expectedResult);
+        });
+      });
+    },
+  );
+});
