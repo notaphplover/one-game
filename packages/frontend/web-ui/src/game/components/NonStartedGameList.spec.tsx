f49a88aa84faa4ff0170b1f87f2710fabb86810c
notaphplover <roberto.pintos.lopez@gmail.com>
2024-06-24 20:47:59 +0200

    refactor(web-ui): add NonStartedGameList component

---
+++ packages/frontend/web-ui/src/game/components/NonStartedGameList.spec.tsx
@@ -0,0 +1,74 @@
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+
+jest.mock('./BaseGameList');
+
+import { models as apiModels } from '@cornie-js/api-models';
+import { RenderResult, render } from '@testing-library/react';
+import React from 'react';
+
+import { BaseGameList, BaseGameListOptions } from './BaseGameList';
+import {
+  NonStartedGameList,
+  NonStartedGameListOptions,
+} from './NonStartedGameList';
+
+describe(NonStartedGameList.name, () => {
+  let optionsFixture: NonStartedGameListOptions;
+
+  beforeAll(() => {
+    optionsFixture = {
+      gamesResult: null,
+    };
+  });
+
+  describe('when called', () => {
+    let baseGameListResultFixture: React.JSX.Element;
+    let baseGameListContentFixture: string;
+
+    let baseGameListContent: string | null | undefined;
+
+    beforeAll(() => {
+      baseGameListContentFixture = 'Expected content fixture';
+      baseGameListResultFixture = (
+        <div className="base-game-list-fixture">
+          {baseGameListContentFixture}
+        </div>
+      );
+
+      (BaseGameList as jest.Mock<typeof BaseGameList>).mockReturnValueOnce(
+        baseGameListResultFixture,
+      );
+
+      const renderResult: RenderResult = render(
+        <NonStartedGameList gamesResult={optionsFixture.gamesResult} />,
+      );
+
+      const baseGameList: ChildNode | undefined =
+        renderResult.container.querySelector('.base-game-list-fixture')
+          ?.childNodes[0];
+
+      baseGameListContent = baseGameList?.nodeValue;
+    });
+
+    afterAll(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should call BaseGameList()', () => {
+      const expectedOptions: BaseGameListOptions = {
+        buildGameItem: expect.any(Function) as unknown as (
+          game: apiModels.GameV1,
+          key: number,
+        ) => React.JSX.Element,
+        gamesResult: optionsFixture.gamesResult,
+      };
+
+      expect(BaseGameList).toHaveBeenCalledTimes(1);
+      expect(BaseGameList).toHaveBeenCalledWith(expectedOptions, {});
+    });
+
+    it('should return expected content', () => {
+      expect(baseGameListContent).toBe(baseGameListContentFixture);
+    });
+  });
+});
---
+++ packages/frontend/web-ui/src/game/components/NonStartedGameList.tsx
@@ -0,0 +1,57 @@
+import { models as apiModels } from '@cornie-js/api-models';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton, Snackbar } from '@mui/material';
+import { Fragment } from 'react';
+
+import { Either } from '../../common/models/Either';
+import useConsecutiveSnackbars from '../hooks/useConsecutiveSnackbars';
+import { BaseGameList } from './BaseGameList';
+import { NonStartedGameListItem } from './NonStartedGameListItem';
+
+const SNACKBAR_MESSAGE_CONTENT: string = 'Link copied to the clipboard';
+
+export interface NonStartedGameListOptions {
+  gamesResult: Either<string, apiModels.GameArrayV1> | null;
+}
+
+function buildGameItemBuilder(
+  enqueue: (messageContent: string) => void,
+): (game: apiModels.GameV1, key: number) => React.JSX.Element {
+  const onClick: () => void = () => enqueue(SNACKBAR_MESSAGE_CONTENT);
+
+  return (game: apiModels.GameV1, key: number) => (
+    <NonStartedGameListItem key={key} game={game} onButtonClick={onClick} />
+  );
+}
+
+export const NonStartedGameList = (options: NonStartedGameListOptions) => {
+  const { close, dequeue, enqueue, message, open } = useConsecutiveSnackbars();
+
+  return (
+    <>
+      <Snackbar
+        action={
+          <Fragment>
+            <IconButton aria-label="close" color="inherit" onClick={close}>
+              <CloseIcon />
+            </IconButton>
+          </Fragment>
+        }
+        anchorOrigin={{
+          horizontal: 'center',
+          vertical: 'top',
+        }}
+        autoHideDuration={3000}
+        className="snackbar"
+        message={message?.content}
+        onClose={close}
+        open={open}
+        TransitionProps={{ onExited: dequeue }}
+      ></Snackbar>
+      <BaseGameList
+        gamesResult={options.gamesResult}
+        buildGameItem={buildGameItemBuilder(enqueue)}
+      />
+    </>
+  );
+};
