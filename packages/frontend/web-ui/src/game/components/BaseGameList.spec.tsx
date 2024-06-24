4b0d82e0e0cc973a6a9223ab448499a957bd456c
notaphplover <roberto.pintos.lopez@gmail.com>
2024-06-24 20:46:48 +0200

    refactor(web-ui): add BaseGameList component

---
+++ packages/frontend/web-ui/src/game/components/BaseGameList.spec.tsx
@@ -0,0 +1,143 @@
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+
+import { models as apiModels } from '@cornie-js/api-models';
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import { Right } from '../../common/models/Either';
+import { BaseGameList } from './BaseGameList';
+
+describe(BaseGameList.name, () => {
+  let buildGameItemMock: jest.Mock<
+    (game: apiModels.GameV1, key: number) => React.JSX.Element
+  >;
+
+  beforeAll(() => {
+    buildGameItemMock = jest.fn();
+  });
+
+  describe('having a null gameResult', () => {
+    describe('when called', () => {
+      let gamesListTextValue: string | null | undefined;
+
+      beforeAll(() => {
+        const renderResult = render(
+          <BaseGameList
+            buildGameItem={buildGameItemMock}
+            gamesResult={null}
+          ></BaseGameList>,
+        );
+
+        const gamesListTextNode: ChildNode | undefined =
+          renderResult.container.querySelector('.game-list-text')
+            ?.childNodes[0];
+
+        gamesListTextValue = gamesListTextNode?.nodeValue;
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should render game list text', () => {
+        expect(gamesListTextValue).toBe('No games found.');
+      });
+    });
+  });
+
+  describe('having a Left gameResult', () => {
+    describe('when called', () => {
+      let gamesListTextValue: string | null | undefined;
+
+      beforeAll(() => {
+        const renderResult = render(
+          <BaseGameList
+            buildGameItem={buildGameItemMock}
+            gamesResult={{
+              isRight: false,
+              value: 'error-message-fixture',
+            }}
+          ></BaseGameList>,
+        );
+
+        const gamesListTextNode: ChildNode | undefined =
+          renderResult.container.querySelector('.game-list-text')
+            ?.childNodes[0];
+
+        gamesListTextValue = gamesListTextNode?.nodeValue;
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should render game list text', () => {
+        expect(gamesListTextValue).toBe(
+          'An error has occurred. Please try again later.',
+        );
+      });
+    });
+  });
+
+  describe('having a Right gameResult', () => {
+    let gameFixture: apiModels.GameV1;
+    let gameResultFixture: Right<apiModels.GameArrayV1>;
+
+    beforeAll(() => {
+      gameFixture = {
+        id: 'id-fixture',
+        state: {
+          slots: [],
+          status: 'nonStarted',
+        },
+      };
+      gameResultFixture = {
+        isRight: true,
+        value: [gameFixture],
+      };
+    });
+
+    describe('when called', () => {
+      let buildGameItemResultFixture: React.JSX.Element;
+      let expectedContent: string;
+
+      let gamesListTextValue: string | null | undefined;
+
+      beforeAll(() => {
+        expectedContent = 'Mock content';
+        buildGameItemResultFixture = (
+          <div className="game-mock" key="game-mock-key">
+            {expectedContent}
+          </div>
+        );
+
+        buildGameItemMock.mockReturnValueOnce(buildGameItemResultFixture);
+
+        const renderResult = render(
+          <BaseGameList
+            buildGameItem={buildGameItemMock}
+            gamesResult={gameResultFixture}
+          ></BaseGameList>,
+        );
+
+        const gamesListTextNode: ChildNode | undefined =
+          renderResult.container.querySelector('.game-mock')?.childNodes[0];
+
+        gamesListTextValue = gamesListTextNode?.nodeValue;
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call buildGameItem()', () => {
+        expect(buildGameItemMock).toHaveBeenCalledTimes(1);
+        expect(buildGameItemMock).toHaveBeenCalledWith(gameFixture, 0);
+      });
+
+      it('should render game list text', () => {
+        expect(gamesListTextValue).toBe(expectedContent);
+      });
+    });
+  });
+});
