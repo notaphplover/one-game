feb0f45f9598d9161c223ac44b6498d1090a5a5d
notaphplover <roberto.pintos.lopez@gmail.com>
2024-06-26 01:14:38 +0200

    refactor(web-ui): add ActiveGameListItem

---
+++ packages/frontend/web-ui/src/game/components/ActiveGameListItem.tsx
@@ -0,0 +1,29 @@
+import { models as apiModels } from '@cornie-js/api-models';
+import { JoinInnerOutlined } from '@mui/icons-material';
+import { Box, Button } from '@mui/material';
+
+import { BaseGameListItem } from './BaseGameListItem';
+
+export interface ActiveGameListItemOptions {
+  game: apiModels.GameV1;
+}
+
+export const ActiveGameListItem = (options: ActiveGameListItemOptions) => {
+  const button = (
+    <Box component="div">
+      <Button
+        className="game-list-item-button"
+        disabled
+        startIcon={<JoinInnerOutlined />}
+        type="button"
+        variant="contained"
+      >
+        Join
+      </Button>
+    </Box>
+  );
+
+  return (
+    <BaseGameListItem button={button} game={options.game}></BaseGameListItem>
+  );
+};
