3f743ceadccf5be60207868e2ca1aa856f613710
notaphplover <roberto.pintos.lopez@gmail.com>
2024-06-30 12:30:49 +0200

    refactor(backend-user-domain): update UserCode with kind

--- packages/backend/apps/user/backend-user-domain/src/users/domain/valueObjects/UserCode.ts
+++ packages/backend/apps/user/backend-user-domain/src/users/domain/valueObjects/UserCode.ts
@@ -1,4 +1,7 @@
+import { UserCodeKind } from './UserCodeKind';
+
 export interface UserCode {
   readonly code: string;
+  readonly kind: UserCodeKind;
   readonly userId: string;
 }
---
+++ packages/backend/apps/user/backend-user-domain/src/users/domain/valueObjects/UserCodeKind.ts
@@ -0,0 +1,4 @@
+export enum UserCodeKind {
+  registerConfirm,
+  resetPassword,
+}
