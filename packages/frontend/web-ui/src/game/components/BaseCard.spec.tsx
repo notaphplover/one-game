9ca1bc20f4eb4df2e99e218c3063fde3dec38423
Mario Fernández <mario.fdez.p@gmail.com>
2024-07-02 17:31:51 +0200

    feat(web-ui): add new component BaseCard and his test

---
+++ packages/frontend/web-ui/src/game/components/BaseCard.spec.tsx
@@ -0,0 +1,46 @@
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
+
+import { RenderResult, render } from '@testing-library/react';
+
+import { BaseCard } from './BaseCard';
+
+describe(BaseCard.name, () => {
+  let childrenMock: jest.Mock<() => React.JSX.Element | React.JSX.Element[]>;
+  let colorClassMock: string;
+
+  beforeAll(() => {
+    childrenMock = jest.fn();
+    colorClassMock = 'blue-card';
+  });
+
+  describe('when called', () => {
+    let existingCardColorClassName: string;
+
+    beforeAll(() => {
+      const renderResult: RenderResult = render(
+        <BaseCard
+          children={childrenMock()}
+          colorClass={colorClassMock}
+        ></BaseCard>,
+      );
+
+      const cardColor: HTMLElement = renderResult.container.querySelector(
+        '.blue-card',
+      ) as HTMLElement;
+
+      console.log(cardColor);
+
+      existingCardColorClassName = window
+        .getComputedStyle(cardColor)
+        .getPropertyValue('display');
+    });
+
+    afterAll(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should show a card with background blue', () => {
+      expect(existingCardColorClassName).not.toBe('none');
+    });
+  });
+});
