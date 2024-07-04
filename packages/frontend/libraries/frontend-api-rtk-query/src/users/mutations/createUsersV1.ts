131d67ed6588f5bb766472a35b59a2219ae0e5f7
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-07-04 21:26:48 +0200

    feat(frontend-api-rtk-query): add createUsersV1 mutation

---
+++ packages/frontend/libraries/frontend-api-rtk-query/src/users/mutations/createUsersV1.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@cornie-js/api-http-client';
+import { models as apiModels } from '@cornie-js/api-models';
+import { AppError, AppErrorKind } from '@cornie-js/frontend-common';
+import { BaseQueryApi } from '@reduxjs/toolkit/query';
+
+import { SerializableAppError } from '../../foundation/error/SerializableAppError';
+import { HttpApiResult } from '../../foundation/http/models/HttpApiResult';
+import {
+  BAD_REQUEST,
+  CONFLICT,
+  OK,
+} from '../../foundation/http/models/httpCodes';
+import { QueryReturnValue } from '../../foundation/http/models/QueryReturnValue';
+import { CreateUsersV1Args } from '../models/CreateUsersV1Args';
+
+type CreateUsersV1Result = HttpApiResult<'createUser'>;
+
+export function createUsersV1(
+  httpClient: HttpClient,
+): (
+  args: CreateUsersV1Args,
+  api: BaseQueryApi,
+) => Promise<QueryReturnValue<apiModels.UserV1, SerializableAppError, never>> {
+  return async (
+    args: CreateUsersV1Args,
+  ): Promise<
+    QueryReturnValue<apiModels.UserV1, SerializableAppError, never>
+  > => {
+    const httpResponse: CreateUsersV1Result =
+      await httpClient.endpoints.createUser({}, ...args.params);
+
+    switch (httpResponse.statusCode) {
+      case OK:
+        return {
+          data: httpResponse.body,
+        };
+      case BAD_REQUEST:
+        return {
+          error: {
+            kind: AppErrorKind.contractViolation,
+            message: httpResponse.body.description,
+          },
+        };
+      case CONFLICT:
+        return {
+          error: {
+            kind: AppErrorKind.entityConflict,
+            message: httpResponse.body.description,
+          },
+        };
+      default:
+        return {
+          error: new AppError(AppErrorKind.unknown),
+        };
+    }
+  };
+}
