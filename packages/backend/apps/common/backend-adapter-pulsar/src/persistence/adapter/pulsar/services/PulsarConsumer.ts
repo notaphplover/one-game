aea13a65d5652ec0c238972f407dc69bb28a1d2d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-07-29 23:00:25 +0200

    feat(backend-adapter-pulsar): update PulsarConsumer with logger

--- packages/backend/apps/common/backend-adapter-pulsar/src/persistence/adapter/pulsar/services/PulsarConsumer.ts
+++ packages/backend/apps/common/backend-adapter-pulsar/src/persistence/adapter/pulsar/services/PulsarConsumer.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, LoggerService } from '@nestjs/common';
 import { Consumer, Message } from 'pulsar-client';
 
 @Injectable()
 export abstract class PulsarConsumer<TMessage> {
   readonly #consumer: Consumer;
+  readonly #logger: LoggerService;
 
   constructor(consumer: Consumer) {
     this.#consumer = consumer;
+    this.#logger = new Logger(PulsarConsumer.name);
   }
 
   public async handleMessages(): Promise<void> {
@@ -14,8 +16,11 @@ export abstract class PulsarConsumer<TMessage> {
       let pulsarMessage: Message;
 
       try {
+        this.#logger.log('Waiting for next message ...');
         pulsarMessage = await this.#consumer.receive();
       } catch (error: unknown) {
+        this.#logger.error(`Unexpected error while receiving message: 
+${this.#stringifyError(error)}`);
         continue;
       }
 
@@ -27,7 +32,13 @@ export abstract class PulsarConsumer<TMessage> {
         await this._handleMessage(message);
         await this.#consumer.acknowledge(pulsarMessage);
       } catch (error: unknown) {
+        this.#logger.error(`Unexpected error while processing message: 
+          ${this.#stringifyError(error)}`);
+
+        this.#logger.log('sending negative ack...');
+
         this.#consumer.negativeAcknowledge(pulsarMessage);
+
         continue;
       }
     }
@@ -35,6 +46,10 @@ export abstract class PulsarConsumer<TMessage> {
 
   protected _messageFromBuffer(data: Buffer): TMessage {
     return JSON.parse(data.toString('utf-8')) as TMessage;
+  }
+
+  #stringifyError(error: unknown): string {
+    return JSON.stringify(error, Object.getOwnPropertyNames(error));
   }
 
   protected abstract _handleMessage(message: TMessage): Promise<void>;
