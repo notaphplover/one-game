a6e9fb8d66f64090e5b0d61799e1cec0ad70558c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-08-04 19:00:37 +0200

    chore(api-json-schemas-provider): update package to rely on eslint 9

--- packages/api/libraries/api-json-schemas-provider/src/readApiJsonSchemas.ts
+++ packages/api/libraries/api-json-schemas-provider/src/readApiJsonSchemas.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
-import path from 'path';
+import path from 'node:path';
 
 import { JsonRootSchema202012 } from '@cornie-js/json-schema-utils';
 import { glob } from 'glob';
