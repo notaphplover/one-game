ef3c76f77eae5b189e3ba8e0b39890ddf089d9e3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-08-24 20:53:04 +0200

    feat(web-ui): remove about us page

--- packages/frontend/web-ui/src/app/routes/CornieAppRoutes.tsx
+++ packages/frontend/web-ui/src/app/routes/CornieAppRoutes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { AboutUsRoutes } from '../../about/routes/AboutUsRoutes';
 import { AuthRoutes } from '../../auth/routes/AuthRoutes';
 import { GameRoutes } from '../../game/routes/GameRoutes';
 import { HomeRoutes } from '../../home/routes/HomeRoutes';
@@ -10,14 +9,10 @@ import { UserRoutes } from '../../user/routes/UserRoutes';
 export const CornieAppRoutes = (): React.JSX.Element => {
   return (
     <Routes>
-      {/* Login and register */}
       <Route path="/auth/*" element={<AuthRoutes />} />
-
-      {/* Cornie App */}
-      <Route path="/about" element={<AboutUsRoutes />} />
-      <Route path="/*" element={<HomeRoutes />} />
       <Route path="/games/*" element={<GameRoutes />} />
       <Route path="/users/*" element={<UserRoutes />} />
+      <Route path="/" element={<HomeRoutes />} />
     </Routes>
   );
 };
