c4d82dca7259220af3cd61a4199df7f9b11092fa
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-08-28 00:12:48 +0200

    refactor(web-ui): simplify home routes

--- packages/frontend/web-ui/src/app/routes/CornieAppRoutes.tsx
+++ packages/frontend/web-ui/src/app/routes/CornieAppRoutes.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../../auth/routes/AuthRoutes';
 import { PageNotFound } from '../../common/pages/PageNotFound';
 import { GameRoutes } from '../../game/routes/GameRoutes';
-import { HomeRoutes } from '../../home/routes/HomeRoutes';
+import { CornieHome } from '../../home/pages/CornieHome';
 import { UserRoutes } from '../../user/routes/UserRoutes';
 
 export const CornieAppRoutes = (): React.JSX.Element => {
@@ -13,7 +13,7 @@ export const CornieAppRoutes = (): React.JSX.Element => {
       <Route path="/auth/*" element={<AuthRoutes />} />
       <Route path="/games/*" element={<GameRoutes />} />
       <Route path="/users/*" element={<UserRoutes />} />
-      <Route path="/" element={<HomeRoutes />} />
+      <Route path="/" element={<CornieHome />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
