8095cad2cbcd8a4fe942cc15d6656f3980319186
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-08-29 00:50:53 +0200

    refactor(web-ui): update PageName with joinGame

--- packages/frontend/web-ui/src/common/helpers/getSlug.ts
+++ packages/frontend/web-ui/src/common/helpers/getSlug.ts
@@ -4,6 +4,7 @@ const NAVBAR_PAGE_NAME_TO_PAGE_SLUG_MAP: { [TPage in PageName]: string } = {
   [PageName.createGame]: '/games',
   [PageName.login]: '/auth/login',
   [PageName.home]: '/',
+  [PageName.joinGame]: '/games/join',
   [PageName.publicGames]: '/games/public',
   [PageName.register]: '/auth/register',
   [PageName.userMe]: '/users/me',
--- packages/frontend/web-ui/src/common/models/PageName.ts
+++ packages/frontend/web-ui/src/common/models/PageName.ts
@@ -1,6 +1,7 @@
 export enum PageName {
   createGame,
   home,
+  joinGame,
   login,
   publicGames,
   register,
