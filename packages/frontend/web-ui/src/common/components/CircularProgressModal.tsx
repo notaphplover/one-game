3e829f8af935aa7f46cefa2de6c993441f4fcd4f
notaphplover <roberto.pintos.lopez@gmail.com>
2024-09-24 12:47:12 +0200

    refactor(web-ui): update CircularProgressModal component without deprecations

--- packages/frontend/web-ui/src/common/components/CircularProgressModal.tsx
+++ packages/frontend/web-ui/src/common/components/CircularProgressModal.tsx
@@ -1,6 +1,6 @@
 import {
+  Box,
   CircularProgress as MuiCircularProgress,
-  Grid,
   Modal,
 } from '@mui/material';
 import React from 'react';
@@ -14,18 +14,9 @@ export const CircularProgressModal = (
 ): React.JSX.Element => {
   return (
     <Modal open={params.open ?? false}>
-      <Grid
-        container
-        className="circular-progress-bck"
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Grid container direction="row" justifyContent="center">
-          <MuiCircularProgress color="primary" />
-        </Grid>
-      </Grid>
+      <Box className="circular-progress-bck">
+        <MuiCircularProgress color="primary" />
+      </Box>
     </Modal>
   );
 };
