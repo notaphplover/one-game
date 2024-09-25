6fb1bc504ba818acdc1142886963cae1f6d45421
notaphplover <roberto.pintos.lopez@gmail.com>
2024-09-25 12:53:46 +0200

    feat(web-ui): update ActiveGameLisitItem with game redirection

--- packages/frontend/web-ui/src/game/components/ActiveGameListItem.spec.tsx
+++ packages/frontend/web-ui/src/game/components/ActiveGameListItem.spec.tsx
@@ -1,10 +1,13 @@
 import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
 
+jest.mock('../../common/helpers/getSlug');
 jest.mock('./BaseGameListItem');
 
 import { models as apiModels } from '@cornie-js/api-models';
 import { render } from '@testing-library/react';
 
+import { getSlug } from '../../common/helpers/getSlug';
+import { PageName } from '../../common/models/PageName';
 import { ActiveGameListItem } from './ActiveGameListItem';
 import { BaseGameListItem, BaseGameListItemOptions } from './BaseGameListItem';
 
@@ -24,11 +27,13 @@ describe(ActiveGameListItem.name, () => {
 
   describe('when called', () => {
     let baseGameListItemContentFixture: string;
+    let slugFixture: string;
 
     let baseGameListItemContent: string | null | undefined;
 
     beforeAll(() => {
       baseGameListItemContentFixture = 'Expected content fixture';
+      slugFixture = '/slug-fixture';
 
       const baseGameListItemFixture = (
         <div className="base-game-list-item-fixture">
@@ -39,6 +44,8 @@ describe(ActiveGameListItem.name, () => {
       (
         BaseGameListItem as jest.Mock<typeof BaseGameListItem>
       ).mockReturnValueOnce(baseGameListItemFixture);
+
+      (getSlug as jest.Mock<typeof getSlug>).mockReturnValueOnce(slugFixture);
 
       const renderResult = render(<ActiveGameListItem game={gameV1Fixture} />);
 
@@ -51,6 +58,11 @@ describe(ActiveGameListItem.name, () => {
 
     afterAll(() => {
       jest.clearAllMocks();
+    });
+
+    it('should call getSlug()', () => {
+      expect(getSlug).toHaveBeenCalledTimes(1);
+      expect(getSlug).toHaveBeenCalledWith(PageName.game);
     });
 
     it('should call BaseGameListItem()', () => {
