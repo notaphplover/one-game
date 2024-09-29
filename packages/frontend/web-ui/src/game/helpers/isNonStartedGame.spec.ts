9aee84818aa00a9573afcb8ae002ef547a48ce78
notaphplover <roberto.pintos.lopez@gmail.com>
2024-09-29 19:55:56 +0200

    refactor(web-ui): add isNonStartedGame helper

---
+++ packages/frontend/web-ui/src/game/helpers/isNonStartedGame.spec.ts
@@ -0,0 +1,59 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { models as apiModels } from '@cornie-js/api-models';
+
+import { isNonStartedGame } from './isNonStartedGame';
+
+describe(isNonStartedGame.name, () => {
+  describe.each<[string, apiModels.GameV1, boolean]>([
+    [
+      'a non started game',
+      {
+        id: 'id-fixture',
+        isPublic: true,
+        state: {
+          slots: [],
+          status: 'nonStarted',
+        },
+      },
+      true,
+    ],
+    [
+      'a non non started game',
+      {
+        id: 'id-fixture',
+        isPublic: true,
+        state: {
+          currentCard: {
+            kind: 'wildDraw4',
+          },
+          currentColor: 'blue',
+          currentDirection: 'antiClockwise',
+          currentPlayingSlotIndex: 1,
+          currentTurnCardsDrawn: true,
+          currentTurnCardsPlayed: true,
+          drawCount: 0,
+          lastEventId: 'last-event-id-fixture',
+          slots: [],
+          status: 'active',
+        },
+      },
+      false,
+    ],
+  ])(
+    'having %s game',
+    (_: string, gameFixture: apiModels.GameV1, expectedResult: boolean) => {
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = isNonStartedGame(gameFixture);
+        });
+
+        it('should return expected result', () => {
+          expect(result).toBe(expectedResult);
+        });
+      });
+    },
+  );
+});
