3019ac06a8bae44651e3c5655e43280bec91c0d0
Mario Fernández <mario.fdez.p@gmail.com>
2024-10-01 17:28:27 +0200

    feat(web-ui): change on interface SerializableAppError

--- packages/frontend/libraries/frontend-api-rtk-query/src/foundation/error/SerializableAppError.ts
+++ packages/frontend/libraries/frontend-api-rtk-query/src/foundation/error/SerializableAppError.ts
@@ -1,6 +1,8 @@
 import { AppErrorKind } from '@cornie-js/frontend-common';
 
-export interface SerializableAppError {
-  kind: AppErrorKind;
+export interface SerializableAppError<
+  TKind extends AppErrorKind = AppErrorKind,
+> {
+  kind: TKind;
   message: string;
 }
