cf38cb6625b5ddadf007051a3269b69c4d41838b
Mario Fernández <mario.fdez.p@gmail.com>
2024-10-05 17:23:45 +0200

    refactor(web-ui): add new function getCreateAuthErrorMessage and his test

---
+++ packages/frontend/web-ui/src/auth/helpers/createAuthErrorMessages.ts
@@ -0,0 +1,6 @@
+export const HTTP_BAD_REQUEST_AUTH_ERROR_MESSAGE: string =
+  'Unexpected error occurred while processing the request.';
+export const HTTP_UNAUTHORIZED_AUTH_ERROR_MESSAGE: string =
+  'Missing credentials.';
+export const UNEXPECTED_AUTH_ERROR_MESSAGE: string =
+  'Unexpected error occurred while processing the request.';
