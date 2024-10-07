c1c3900efc7a209c58fb71e16a521ba0135169b5
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-10-08 00:18:11 +0200

    refactor(frontend-api-rtk-query): add GetGamesGameIdSpecsV1Args

---
+++ packages/frontend/libraries/frontend-api-rtk-query/src/games/models/GetGamesGameIdSpecsV1Args.ts
@@ -0,0 +1,5 @@
+import { HttpApiParamsWithNoHeaders } from '../../foundation/http/models/HttpApiParamsWithNoHeaders';
+
+export interface GetGamesGameIdSpecsV1Args {
+  params: HttpApiParamsWithNoHeaders<'getGameGameIdSpec'>;
+}
